fc9d04e38df4b9da7530e4c08d39580a6ac0818b
Sudhanshu09102000 <127600086+Sudhanshu09102000@users.noreply.github.com>
2025-12-18 01:46:48 +0530

    Add files via upload

---
+++ scripts/bio2.js
@@ -0,0 +1,201 @@
+const questions={
+    1: {
+  text: "Which type of muscles do the uterus, iris of the eye, and bronchi contain? [SSC PHASE XII 20/06/2024]",
+  options: ["Skeletal muscles", "Smooth muscles", "Striated muscles", "Cardiac muscles"],
+  correct: "2",
+  image: ""
+},
+2: {
+  text: "How big are red blood cells in diameter? [SSC CPO 04/10/2023]",
+  options: ["2.0 µm", "4.0 µm", "7.5 µm", "40.0 µm"],
+  correct: "3",
+  image: ""
+},
+3: {
+  text: "Which tissue among the following is a protective tissue in the animal body? [SSC MTS 20/10/2021]",
+  options: ["Muscular tissue", "Nervous tissue", "Connective tissue", "Epithelial tissue"],
+  correct: "4",
+  image: ""
+},
+4: {
+  text: "Which of the following is NOT a type of blood cell made by the bone marrow? [SSC MTS 05/10/2021]",
+  options: ["Osteoclasts", "Red blood cells", "Platelets", "White blood cells"],
+  correct: "1",
+  image: ""
+},
+5: {
+  text: "The blood platelets are also known as _. [SSC MTS 16/06/2023]",
+  options: ["thrombin", "thrombocytes", "erythrocytes", "leucocytes"],
+  correct: "2",
+  image: ""
+},
+6: {
+  text: "Which of the following is the largest dental tissue? [SSC CHSL 11/08/2021]",
+  options: ["Dentin", "Pulp", "Enamel", "Root"],
+  correct: "1",
+  image: ""
+},
+7: {
+  text: "Which of the following microscopic cells are of an amoeboid shape with a distinctive central membrane? [SSC CHSL 06/06/2022]",
+  options: ["Nerve cells", "White blood cells", "Guard cells", "Red blood cells"],
+  correct: "2",
+  image: ""
+},
+8: {
+  text: "How do the muscle cells help in movement? [SSC CHSL 02/07/2024]",
+  options: ["The lining of the vessels inside help in movement.", "The contraction and relaxation of these cells result in movement.", "The blood flow of the cells helps in movement.", "The thickness of the cell layer helps in movement."],
+  correct: "2",
+  image: ""
+},
+9: {
+  text: "Which of the following is a large bean-shaped lymphoid organ in the human body? [SSC CHSL 16/04/2021]",
+  options: ["Thymus", "Spleen", "Lymph node", "Tonsil"],
+  correct: "2",
+  image: ""
+},
+10: {
+  text: "The fluid present between joint that makes the movement of joints easy, is called: [SSC MTS 08/10/2021]",
+  options: ["Bursae", "Synovial", "Cartilage", "Tendon"],
+  correct: "2",
+  image: ""
+},
+11: {
+  text: "Adipose tissue is a type of _ tissue in the human body. [SSC GD 14/12/2021]",
+  options: ["Epithelial", "Muscular", "Connective", "Nervous"],
+  correct: "3",
+  image: ""
+},
+12: {
+  text: "Which is a reversible, fluorescent red, acidic dye used to stain cytoplasm, red blood cells, collagen and muscle fibres for histological examination? [SSC STENOGRAPHER 12/10/2023]",
+  options: ["Eosin", "Safranin", "Crystal violet", "Alizarin"],
+  correct: "1",
+  image: ""
+},
+13: {
+  text: "Muscles are of three types. Which of the following is NOT one of them? [SSC CHSL 16/03/2023]",
+  options: ["Sesamoids", "Smooth", "Cardiac", "Skeletal"],
+  correct: "1",
+  image: ""
+},
+14: {
+  text: "Which of the following is a type of connective tissue? [SSC CHSL 17/03/2023]\nI. Blood\nII. Bone\nIII. Ligament",
+  options: ["Only III", "Only II and III", "I, II and III", "Only I and II"],
+  correct: "3",
+  image: ""
+},
+15: {
+  text: "Which one of the following is not a connective tissue? [CAPF 2021]",
+  options: ["Ligament", "Tendon", "Smooth muscle", "Cartilage"],
+  correct: "3",
+  image: ""
+},
+16: {
+  text: "In some viral infections, number of platelets are reduced quickly. Platelets are essential because [CAPF 2023]",
+  options: ["they along with red blood cells carry oxygen.", "platelets are important to carry food to cells through blood.", "platelets remove carbon dioxide from blood.", "platelets have a role in blood clotting."],
+  correct: "4",
+  image: ""
+},
+17: {
+  text: "The lymph is mainly formed from the plasma of blood and it surrounds the cells. The main difference between blood and lymph is: [CAPF 2023]",
+  options: ["Lymph does not contain red blood cells.", "Lymph does not contain white blood cells.", "Lymph contains both red blood cells and white blood cells.", "Lymph does not contain any cells."],
+  correct: "1",
+  image: ""
+},
+18: {
+  text: "Which one of the following is the structure of a cardiac muscle cell? [CDS I 2021]",
+  options: ["Cylindrical, Unbranched and Multinucleate", "Spinal shaped, Unbranched and Uninucleate", "Spinal shaped, Branched and Uninucleate", "Cylindrical, Branched and Uninucleate"],
+  correct: "4",
+  image: ""
+},
+19: {
+  text: "Suppose, due to an accident, the delicate lining of the mouth and the oesophagus of a person is injured. In this case, which one of the following tissues will be affected? [CDS I 2022]",
+  options: ["Smooth muscle", "Cuboidal epithelium", "Squamous epithelium", "Striated muscle"],
+  correct: "3",
+  image: ""
+},
+20: {
+  text: "Fat in human body is stored in [CDS II 2022]",
+  options: ["Adipose tissue", "Cuboidal epithelium", "Muscle tissue", "Striated muscle"],
+  correct: "1",
+  image: ""
+},
+21: {
+  text: "Which cells in human body have no nucleus? [CDS I 2024]",
+  options: ["Muscle cells", "Phagocytic cells", "Red blood cells", "Ciliated cells"],
+  correct: "3",
+  image: ""
+},
+22: {
+  text: "From which among the following are blood clotting factors released? [CDS I 2024]",
+  options: ["RBCs", "Eosinophils", "Platelets", "Monocytes"],
+  correct: "3",
+  image: ""
+},
+23: {
+  text: "The tissue which is responsible for movement in our body is [CISF AC 2021]",
+  options: ["Connective tissue", "Muscular tissue", "Nervous tissue", "Epithelial tissue"],
+  correct: "2",
+  image: ""
+},
+24: {
+  text: "Tendons connect: [GEOSCIENTIST 2021]",
+  options: ["Bone to bone", "Bone to cartilage", "Muscle to bone", "Cartilage to fat"],
+  correct: "3",
+  image: ""
+},
+25: {
+  text: "Tissue is formed from cells in which: [GEOSCIENTIST 2021]",
+  options: ["most cells are similar in structure and function.", "all cells perform different functions.", "all cells are different in structure.", "all cells are different in both structure and function."],
+  correct: "1",
+  image: ""
+},
+26: {
+  text: "Which one of the following sets of elements is essential for the production of red blood cells in human body? [GEOSCIENTIST 2022]",
+  options: ["Arsenic and zinc only", "Iron and zinc only", "Arsenic and iron only", "Arsenic, iron, lead and zinc"],
+  correct: "2",
+  image: ""
+},
+27: {
+  text: "Squamous epithelial cells are found in the inner lining of [NDA I 2021]",
+  options: ["Oesophagus.", "Small intestine.", "Ducts of salivary gland.", "Kidney."],
+  correct: "1",
+  image: ""
+},
+28: {
+  text: "Lymph is a tissue fluid present in intercellular spaces. It resembles to [NDA II 2022]",
+  options: ["digestive juice", "cytoplasmic fluid", "urine", "plasma"],
+  correct: "4",
+  image: ""
+},
+29: {
+  text: "Which one of the following is the correct sequence in increasing complexity? [NDA I 2024]",
+  options: ["Protein - Organ - Tissue - Organ", "Protein - Organ - Tissue - Organism", "Protein - Organism - Organ - Tissue", "Protein - Tissue - Organ - Organism"],
+  correct: "4",
+  image: ""
+},
+30: {
+  text: "Which one among the following cells produces antibodies against a foreign antigen? [NDA I 2024]",
+  options: ["Lymphocytes", "Erythrocytes", "Eosinophils", "Platelets"],
+  correct: "1",
+  image: ""
+},
+31: {
+  text: "Which one among the following tissues help to connect two bones? [NDA II 2024]",
+  options: ["Tendons", "Ligaments", "Areolar tissue", "Cartilage"],
+  correct: "2",
+  image: ""
+},
+32: {
+  text: "Consider the following cell types: [NDA I 2024]\n1. Monocyte\n2. Chondrocyte\n3. Basophil\n4. Lymphocyte\nHow many of the above belong to animal cell types?",
+  options: ["1", "2", "3", "4"],
+  correct: "4",
+  image: ""
+},
+33: {
+  text: "Clotting of blood involves which one among the following clotting proteins? [NDA I 2025]",
+  options: ["Pathogen", "Fibrinogen", "Macrophage", "Phagocytes"],
+  correct: "2",
+  image: ""
+}
+
+}
