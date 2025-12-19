d4e7c755517526c30454c9036181158a1493b045
Sudhanshu09102000 <127600086+Sudhanshu09102000@users.noreply.github.com>
2025-12-20 03:39:24 +0530

    Add files via upload

---
+++ scripts/bio8.js
@@ -0,0 +1,383 @@
+const questions={
+  // Level I Questions (1–38)
+1: {
+  text: "Who described the algae Spirogyra in 1674 and named the motile organisms Animalcules, meaning 'little animals'?",
+  options: ["Barthelemy Dumortier", "Anton Van Leeuwenhoek", "Maurice Wilkins", "Robert Remak"],
+  correct: "2",
+  image: ""
+},
+2: {
+  text: "Who explained the structure and reproductive system of algae in 1935? [SSC CGL TIER-II 06/03/2023]",
+  options: ["M.O.P. Iyengar", "F.E. Fritsch", "William Henry", "Carolus Linnaeus"],
+  correct: "2",
+  image: ""
+},
+3: {
+  text: "The reproduction process in which only a single parent is involved is called _______. [SSC MTS 11/05/2023]",
+  options: ["In vitro fertilization", "External fertilization", "Sexual reproduction", "Asexual reproduction"],
+  correct: "4",
+  image: ""
+},
+4: {
+  text: "The innermost part of a flower is called the ____. [SSC MTS 12/05/2023]",
+  options: ["Stamens", "Pistil", "Petals", "Sepals"],
+  correct: "2",
+  image: ""
+},
+5: {
+  text: "Which part of a flower is the sticky surface at the top of the pistil, it traps and holds the pollen? [SSC MTS 07/10/2021]",
+  options: ["Style", "Stigma", "Sepal", "Ovary"],
+  correct: "2",
+  image: ""
+},
+6: {
+  text: "What is the yellow dust present in the middle of a flower called? [SSC CGL 09/03/2020]",
+  options: ["Stigma", "Stamen", "Pistil", "Style"],
+  correct: "2",
+  image: ""
+},
+7: {
+  text: "Which type of reproduction occurs in hydra? [SSC MTS 08/08/2019]",
+  options: ["Fragmentation", "Budding", "Binary Fission", "Spore Formation"],
+  correct: "2",
+  image: ""
+},
+8: {
+  text: "Which of the following is considered unicellular green algae? [SSC MTS 16/10/2024]",
+  options: ["Chlorophyta", "Cladophora", "Chlamydomonas", "Oedogonium"],
+  correct: "3",
+  image: ""
+},
+9: {
+  text: "Which of the following pairs of microorganism type is correct?\n I. Paramecium - Fungi\n II. Penicillium - Protozoa [SSC CPO 10/11/2022]",
+  options: ["Only II", "Both I and II", "Neither I nor II", "Only I"],
+  correct: "3",
+  image: ""
+},
+10: {
+  text: "How do Pteridophytes reproduce? [SSC CPO 12/12/2019]",
+  options: ["With the help of budding", "With the help of spores", "With the help of pollens", "With the help of seeds"],
+  correct: "2",
+  image: ""
+},
+11: {
+  text: "The development of a fruit without fertilization is called? [SSC CGL 04/03/2020]",
+  options: ["Gametogamy", "Parthenocarpy", "Hybridogenesis", "Apomixis"],
+  correct: "2",
+  image: ""
+},
+12: {
+  text: "Which of the following are flowerless plants that produce cones and seeds in which seeds are not encased within an ovary? [SSC CHSL 03/07/2019]",
+  options: ["Gymnosperms", "Angiosperms", "Bryophytes", "Hydrophytes"],
+  correct: "1",
+  image: ""
+},
+13: {
+  text: "A type of asexual reproduction in which new plants are produced from roots, stems, leaves, and buds are known as: [SSC MTS 26/10/2021]",
+  options: ["Layering propagation", "Grafting propagation", "Budding propagation", "Vegetative propagation"],
+  correct: "4",
+  image: ""
+},
+14: {
+  text: "How do the red algae reproduce vegetatively? [SSC CHSL 25/05/2022]",
+  options: ["Fission", "Spore formation", "Cutting", "Fragmentation"],
+  correct: "4",
+  image: ""
+},
+15: {
+  text: "Which chromosome is involved in the production of 'masked' mRNAs for early development? [SSC PHASE XII 21/06/2024]",
+  options: ["Lampbrush chromosome", "Polytene chromosome", "Autosomal chromosome", "Sex chromosome"],
+  correct: "1",
+  image: ""
+},
+16: {
+  text: "Some simple multicellular organisms reproduce by first dividing into several parts. Then each of these parts grows and becomes a new organism. What is this method of reproduction called? [SSC JE 06/06/2024]",
+  options: ["Reproduction", "Binary Fission", "Multiple Fission", "Fragmentation"],
+  correct: "4",
+  image: ""
+},
+17: {
+  text: "Select the option that shows the correct matching of Column I with Column II regarding Asexual Reproduction. [RRB GROUP D 24/08/2022]",
+  options: ["a-ii, b-iv, c-iii, d-i", "a-ii, b-i, c-iii, d-iv", "a-i, b-ii, c-iii, d-iv", "a-iv, b-ii, c-i, d-iii"],
+  correct: "1",
+  image: "attached_image:1"
+},
+18: {
+  text: "Identify an algae that undergoes anisogamous fusion of gametes. [SSC PHASE XI 28/06/2023]",
+  options: ["Spirogyra", "Eudorina", "Volvox", "Ulothrix"],
+  correct: "2",
+  image: ""
+},
+19: {
+  text: "Which of the following is not an asexual form of reproduction in plants? [SSC CHSL 10/03/2023]",
+  options: ["Spore formation", "Vegetative propagation", "Budding", "Pollination"],
+  correct: "4",
+  image: ""
+},
+20: {
+  text: "Which of the following statements regarding reproduction in flowering plants is FALSE? [RRB GROUP D 29/09/2022]",
+  options: ["Reproduction may occur through seeds.", "Reproduction may occur from fragments of stems.", "Reproduction may occur through spores.", "Reproduction may occur from tissues separated from the plant and grown on an artificial medium."],
+  correct: "3",
+  image: ""
+},
+21: {
+  text: "Reproduction in humans occurs through which type of fertilisation? [SSC MTS 14/08/2019]",
+  options: ["Internal Fertilisation", "In Vitro Fertilisation", "External Fertilisation", "Ex Vitro Fertilisation"],
+  correct: "1",
+  image: ""
+},
+22: {
+  text: "In humans, fertilisation usually takes place in the ______. [SSC CPO 12/03/2019]",
+  options: ["Ovary", "Vagina", "Fallopian Tube", "Uterus"],
+  correct: "3",
+  image: ""
+},
+23: {
+  text: "During fertilisation in living beings, fusion of male and female gametes takes place to form a cell called ______. [SSC CPO 13/12/2019]",
+  options: ["Ovum", "Sperm", "Embryo", "Zygote"],
+  correct: "4",
+  image: ""
+},
+24: {
+  text: "_____ is the ability of a single cell to produce a fertile, adult individual. [SSC CGL 17/08/2021]",
+  options: ["Pluripotency", "Totipotency", "Cloning", "Mutation"],
+  correct: "2",
+  image: ""
+},
+25: {
+  text: "How many arteries are there in an umbilical cord? [SSC CGL 13/08/2021]",
+  options: ["One", "Two", "Four", "Three"],
+  correct: "2",
+  image: ""
+},
+26: {
+  text: "Which of the following is the primary sex organ in females? [SSC CGL 06/03/2020]",
+  options: ["Fallopian tube", "Ovary", "Uterus", "Vagina"],
+  correct: "2",
+  image: ""
+},
+27: {
+  text: "All human beings have _____ pairs of chromosomes in the nuclei of their cells. [SSC MTS 19/06/2023]",
+  options: ["11", "41", "33", "23"],
+  correct: "4",
+  image: ""
+},
+28: {
+  text: "The stage of the embryo in which all the body parts can be identified is called ____. [SSC MTS 09/05/2023]",
+  options: ["Uterus", "Ovulation", "Foetus", "Zygote"],
+  correct: "3",
+  image: ""
+},
+29: {
+  text: "The phenomenon whereby the female gamete undergoes development to form new organisms without fertilisation in some organisms like honey bees, some lizards, and birds is called: [SSC CPO 25/11/2020]",
+  options: ["Budding", "Fragmentation", "Apomixis", "Parthenogenesis"],
+  correct: "4",
+  image: ""
+},
+30: {
+  text: "Identify an organism that exhibits metagenesis. [SSC CGL 26/07/2023]",
+  options: ["Obelia", "Spongilla", "Sycon", "Euspongia"],
+  correct: "1",
+  image: ""
+},
+31: {
+  text: "The period of life, when the body undergoes changes leading to reproductive maturity is called: [SSC MTS 04/05/2023]",
+  options: ["Maturity", "Adolescence", "Cloning", "Budding"],
+  correct: "2",
+  image: ""
+},
+32: {
+  text: "Which of the following is NOT an example of \"viviparous\" animals? [SSC MTS 14/06/2023]",
+  options: ["Bird", "Dog", "Cat", "Cow"],
+  correct: "1",
+  image: ""
+},
+33: {
+  text: "Which of the creatures is oviparous? [SSC CGL 11/06/2019]",
+  options: ["Frog", "Rabbit", "Mouse", "Squirrel"],
+  correct: "1",
+  image: ""
+},
+34: {
+  text: "The transformation of the larva into an adult through drastic change is called ______. [SSC MTS 14/06/2023]",
+  options: ["Budding", "Shifting", "Bulging", "Metamorphosis"],
+  correct: "4",
+  image: ""
+},
+35: {
+  text: "Which of the following statements refers to the process of \"Parthenogenesis\"? [IB ACIO 18/02/2021]",
+  options: ["It is a natural form of asexual reproduction in which growth and development of embryos occur without fertilization.", "It is a mode of reproduction in which a male outside's sperm fertilizes a female organism's egg outside of the female's body.", "It is a mode of reproduction in which a male organism's sperm fertilizes a female organism's egg inside of the female's body.", "Select the correct answer using the codes given below."],
+  correct: "1",
+  image: ""
+},
+36: {
+  text: "In biology, the term \"IVF\" stands for ____. [SSC MTS 02/05/2023]",
+  options: ["In Vitro Fragmentation", "In Vitro Fertilisation", "Internal Venom Frosting", "Internal Vitro Fragmentation"],
+  correct: "2",
+  image: ""
+},
+37: {
+  text: "Which part of the human body has the growth stages divided into - Anagen, Catagen, and Telogen? [SSC CHSL 15/04/2021]",
+  options: ["Hair", "Tooth", "Nose", "Nails"],
+  correct: "1",
+  image: ""
+},
+38: {
+  text: "A flow chart of sex determination in human beings is shown below. Some conclusions that can be drawn from it are mentioned in the options. Select the INCORRECT option. [RRB GROUP D 26/08/2022]",
+  options: ["All children inherit X chromosome from their mother.", "The chances of a child being male or female is 50%.", "The male decides the sex of the offspring.", "The female decides the sex of the offspring."],
+  correct: "4",
+  image: "attached_image:2"
+},
+// Level II Questions (Start as numbering 39)
+39: {
+  text: "Buds produced in the notches along the leaf margin of Bryophyllum fall on soil and develop into new plants. This is an example of which one of the following types of reproduction? [CAPF 2022]",
+  options: ["Vegetative propagation", "Budding", "Spore formation", "Regeneration"],
+  correct: "1",
+  image: ""
+},
+40: {
+  text: "A child receives a tall beautiful plant as a birthday gift from his father with a quiz. The father asked her how she would verify whether this tall plant was the progeny of both the tall parents or one tall and one short parent plant. She could verify this through [CDS I 2021]",
+  options: ["cross-pollination", "self-pollination", "tissue culture", "negative propagation"],
+  correct: "2",
+  image: ""
+},
+41: {
+  text: "Which one of the following statements about variations is not true? [CDS II 2021]",
+  options: ["Variation is minimum in asexual reproduction.", "All variations in a species have equal chances of survival.", "Changes in genetic constitution result in variation.", "Variants can be selected by environmental factors."],
+  correct: "2",
+  image: ""
+},
+42: {
+  text: "Hyphae are: [CDS I 2022]",
+  options: ["produced by yeast", "thin threads formed by many fungi", "reproductive cells", "responsible for preventing the cell from losing water in wet conditions"],
+  correct: "2",
+  image: ""
+},
+43: {
+  text: "Asexual reproduction takes place through budding in [CISF AC 2021]",
+  options: ["E. coli", "Amoeba", "Plasmodium", "Yeast"],
+  correct: "4",
+  image: ""
+},
+44: {
+  text: "Fruit formation from the thalamus instead of ovary is known as false fruit. The most common examples are [CISF AC 2021]",
+  options: ["Orange and mango", "Mango and banana", "Apple and strawberry", "Guava and cherry"],
+  correct: "3",
+  image: ""
+},
+45: {
+  text: "Which one of the following is a 'false fruit'? [GEOSCIENTIST 2023]",
+  options: ["Peach", "Banana", "Apple", "Apricot"],
+  correct: "3",
+  image: ""
+},
+46: {
+  text: "Which one of the following is a 'drupe'? [GEOSCIENTIST 2023]",
+  options: ["Orange", "Brinjal", "Coconut", "Tomato"],
+  correct: "3",
+  image: ""
+},
+47: {
+  text: "What is the number of chromosomes found in human somatic cell? [GEOSCIENTIST 2023]",
+  options: ["48", "56", "46", "42"],
+  correct: "3",
+  image: ""
+},
+48: {
+  text: "Which one of the following animals lays eggs to reproduce? [GEOSCIENTIST 2023]",
+  options: ["Dolphin", "Seal", "Platypus", "Whale"],
+  correct: "3",
+  image: ""
+},
+49: {
+  text: "Which one of the following is the technique of joining parts of two plants to form a composite plant? [GEOSCIENTIST 2024]",
+  options: ["Layering", "Grafting", "Cutting", "Budding"],
+  correct: "2",
+  image: ""
+},
+50: {
+  text: "Many fruits burst with a sudden jerk because of which the seeds are scattered a few yards from the parent plant. This type of dispersal of seeds is known as: [GEOSCIENTIST 2024]",
+  options: ["Anemochory", "Hydrochory", "Autochory", "Zoochory"],
+  correct: "3",
+  image: ""
+},
+51: {
+  text: "In a typical flower, germinating pollen grains pass through several parts of the gynoecium before they reach the ovule. A list of the parts of gynoecium is given below in different combinations. Choose the combination that represents the correct sequence of pollen tube pathway/journey: [NDA I 2021]",
+  options: ["Style, Stigma, Ovary", "Stigma, Style, Ovary", "Pistil, Stigma, Ovary", "Ovary, Pistil, Style"],
+  correct: "2",
+  image: ""
+},
+52: {
+  text: "Which one of the following is the mechanism of action of oral contraceptive pills? [NDA I 2022]",
+  options: ["They kill the egg.", "They kill the sperm.", "They kill the zygote.", "They inhibit the release of egg."],
+  correct: "4",
+  image: ""
+},
+53: {
+  text: "Which one of the following plants has unisexual flowers? [NDA I 2022]",
+  options: ["Papaya", "Hibiscus", "Mustard", "Sunflower"],
+  correct: "1",
+  image: ""
+},
+54: {
+  text: "After fertilization, the fruit and the seed are produced by [NDA II 2022]",
+  options: ["ovule and ovary, respectively", "ovary and ovule, respectively", "ovary, no ovule required", "ovule, no ovary required"],
+  correct: "2",
+  image: ""
+},
+55: {
+  text: "One advantage of sexual reproduction over asexual reproduction is that it helps species to survive over long evolutionary time. This is because sexual reproduction produces: [NDA I 2023]",
+  options: ["more offspring in each reproductive cycle.", "robust and healthy offspring.", "genetically similar offspring.", "more variation in offspring."],
+  correct: "4",
+  image: ""
+},
+56: {
+  text: "The two important features of sexual reproduction in higher organisms that create genetic diversity in offspring are [NDA II 2023]",
+  options: ["Mitosis and fertilization", "Meiosis and fertilization", "Mitosis and binary fission", "Meiosis and conjugation"],
+  correct: "2",
+  image: ""
+},
+57: {
+  text: "In angiosperms, pollen grain germinates to produce two male gametes. Which one of the following functions is carried out by these gametes? [NDA II 2023]",
+  options: ["Both the gametes fuse with a single egg cell.", "Both the gametes fuse with two different egg cells.", "One gamete fuses with the egg cell and the other one eventually degenerate.", "One gamete fuses with the egg cell and the other one fuses with a diploid secondary nucleus."],
+  correct: "4",
+  image: ""
+},
+58: {
+  text: "In a sexually reproducing organism, which one of the following statements is appropriate both for the parent and offspring? [NDA II 2023]",
+  options: ["Chromosome number increases but DNA content remains constant.", "Both chromosome number and DNA content remains constant.", "Chromosome number decreases but DNA content remains constant.", "Both chromosome number and DNA content decreases."],
+  correct: "2",
+  image: ""
+},
+59: {
+  text: "Relationship between a flower and honey-bee will help the flower for/in [NDA II 2023]",
+  options: ["Faster development", "Pollination", "Quick germination of pollens", "Increase in size"],
+  correct: "2",
+  image: ""
+},
+60: {
+  text: "In flowering plants, DNA content of the parent plant gets halved during [NDA II 2023]",
+  options: ["Seed germination", "Fruit formation", "Flower bud formation", "Pollen formation"],
+  correct: "4",
+  image: ""
+},
+61: {
+  text: "Which one of the following is the correct sequence during sexual reproduction in Angiosperms? [NDA II 2023]",
+  options: ["Egg \u2192 zygote \u2192 embryo \u2192 seed", "Embryo \u2192 egg \u2192 zygote \u2192 seed", "Egg \u2192 embryo \u2192 zygote \u2192 seed", "Egg \u2192 seed \u2192 zygote \u2192 embryo"],
+  correct: "1",
+  image: ""
+},
+62: {
+  text: "Which one of the following combinations of events represents the correct sequence during reproduction flowering plants? [NDA II 2023]",
+  options: ["Ovule \u2192 fruit; egg \u2192 embryo; zygote \u2192 seed; ovary \u2192 egg", "Embryo \u2192 egg; ovary \u2192 fruit; ovule \u2192 zygote; zygote \u2192 seed", "Ovary \u2192 fruit; zygote \u2192 egg; embryo \u2192 ovule; seed \u2192 fruit", "Egg \u2192 zygote; zygote \u2192 embryo; ovule \u2192 seed; ovary \u2192 fruit"],
+  correct: "4",
+  image: ""
+},
+63: {
+  text: "In which one among the following the vegetative propagation takes place through 'Eye Buds'? [NDA II 2024]",
+  options: ["Potato", "Ginger", "Onion", "Sugarcane"],
+  correct: "1",
+  image: ""
+}
+
+}
