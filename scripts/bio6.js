509e031e6fdb2bc4701fd7e32129e366cfe70629
Sudhanshu09102000 <127600086+Sudhanshu09102000@users.noreply.github.com>
2025-12-20 02:39:10 +0530

    Add files via upload

---
+++ scripts/bio6.js
@@ -0,0 +1,99 @@
+const questions={
+  1: {
+  text: "Identify the type of cell that is long and branched. [SSC PHASE XI 28/06/2023]",
+  options: ["Nerve cell", "Columnar epithelial cell", "White blood cell", "Red blood cell"],
+  correct: "1",
+  image: ""
+},
+2: {
+  text: "The vertebrae are a part of which of the following body systems In humans? [SSC CGL 17/08/2021]",
+  options: ["Kidney", "Lungs", "Heart", "Spinal cord"],
+  correct: "4",
+  image: ""
+},
+3: {
+  text: "Which part of the brain is instrumental in making it possible for a person to perform an activity such as picking up a pencil from the floor? [SSC CPO 24/11/2020]",
+  options: ["Hypothalamus", "Cerebellum", "Cerebrum", "Cranium"],
+  correct: "2",
+  image: ""
+},
+4: {
+  text: "Glucagon, a peptide hormone, is produced by the__________. [SSC CGL 20/08/2021]",
+  options: ["Pancreas", "Hypothalamus", "Adrenals", "Pituitary glands"],
+  correct: "1",
+  image: ""
+},
+5: {
+  text: "\"Goitre\" is a disease of which of the following glands? [SSC MTS 20/06/2023]",
+  options: ["Thyroid gland", "Pancreas", "Adrenal gland", "Pituitary gland"],
+  correct: "1",
+  image: ""
+},
+6: {
+  text: "Our brain mainly depends on which of the following foods? [SSC PHASE X 02/08/2022]",
+  options: ["Fat and proteins", "Glucose and amino acids", "Only fat", "Only glucose"],
+  correct: "4",
+  image: ""
+},
+7: {
+  text: "Which part of the human brain is responsible for walking in a straight line? [SSC CHSL 20/03/2023]",
+  options: ["Medulla", "Cerebrum", "Pons", "Cerebellum"],
+  correct: "4",
+  image: ""
+},
+8: {
+  text: "The sensation of feeling full is because of a center associated with hunger, which is in a separate part of the _________. [SSC CHSL 21/03/2023]",
+  options: ["Pituitary gland", "Midbrain", "Forebrain", "Hindbrain"],
+  correct: "3",
+  image: ""
+},
+9: {
+  text: "Which one of the following is a degenerative disorder of the brain and one of several conditions that cause dementia, a progressive decline of mental functions resulting in memory loss and confusion? [SSC CHSL 04/07/2019]",
+  options: ["Schizophrenia", "Alzheimer's Disease", "Amnesia", "Concussion"],
+  correct: "2",
+  image: ""
+},
+10: {
+  text: "In 1873, who published the first photograph of nerve tissue stained with the black reaction technique, describing a whole nerve cell? [SSC MTS 01/09/2023]",
+  options: ["Gunter Blobel", "Camillo Golgi", "William Harvey", "Paul Ehrlich"],
+  correct: "2",
+  image: ""
+},
+11: {
+  text: "With whom did Camillo Golgi receive the Nobel Prize in 1906 for his work on the structure of the nervous system? [SSC CHSL 07/06/2022]",
+  options: ["Lina Aletti", "Cesare Lombroso", "Ettore Marchiafava", "Santiago Ramony Cajal"],
+  correct: "4",
+  image: ""
+},
+12: {
+  text: "The hypothalamus, a small region of the _______ plays a crucial role in many important functions of the human body. [SSC CPO 13/03/2019]",
+  options: ["Stomach", "Liver", "Kidney", "Brain"],
+  correct: "4",
+  image: ""
+},
+13: {
+  text: "Which of the following is NOT a part of the hind-brain? [SSC MTS 07/10/2021]",
+  options: ["Pons", "Medulla", "Cerebellum", "Hypothalamus"],
+  correct: "4",
+  image: ""
+},
+14: {
+  text: "Reflex arcs are evolved in animals for quick and efficient responses. Which one of the following sequences correctly represents a reflex arc? [CDS I 2023]",
+  options: ["Receptor - Sensory neuron - Relay neuron in spinal cord - Brain - Motor neuron - Effector", "Receptor - Sensory neuron - Brain - Relay neuron in spinal cord - Motor neuron - Effector", "Receptor - Motor neuron - Relay neuron in spinal cord - Sensory neuron - Effector", "Receptor - Motor neuron - Brain - Sensory neuron - Effector"],
+  correct: "1",
+  image: ""
+},
+15: {
+  text: "The cells associated with receiving and conducting impulses are: [GEOSCIENTIST 2021]",
+  options: ["Neuron", "Plasma cells", "Adipocytes", "Macrophages"],
+  correct: "1",
+  image: ""
+},
+16: {
+  text: "The transfer of electrical signals by nerve cells in human body is enabled by [NDA II 2022]",
+  options: ["Sodium", "Potassium", "Iron", "Sodium and Potassium"],
+  correct: "4",
+  image: ""
+}
+
+}
---
+++ scripts/bio6exp.js
@@ -0,0 +1,67 @@
+const explanations={
+  1: {
+  text: "Nerve cell or neuron is the <b>longest cell in the human body</b> and basic unit of nervous system. Neuron receive and transmit signals throughout the body in form of electrical impulses. These electrical impulses release a chemical \"<b>neurotransmitter</b>\" that carries the information to brain cells.",
+  image: ""
+},
+2: {
+  text: "Spinal cord is made up of neurons which helps it to carry information from body to brain and commands from brain to body. Spinal cord is protected by vertebrae (small bones). These vertebras form spine or backbone which consists <b>33 vertebrae</b> and provide structure and support to body. Spinal cord is located inside backbone. <b>Coccyx bone</b> is located at the end of spine.",
+  image: ""
+},
+3: {
+  text: "Cerebellum is a part of <b>hind brain</b>. It is located between cerebrum and base of the brain (brain stem). It is also known as \"<b>little brain</b>\". It's main function is to maintain posture and <b>balance</b> of the body. It also controls precise muscle movements.",
+  image: ""
+},
+4: {
+  text: "Pancreas is a <b>mixed</b> gland. It secretes glucagon by alpha cells. It also secrets insulin hormone (by beta-cells) and regulates sugar (glucose) level in blood. <b>Low</b> glucose level can cause <b>hypoglycemia</b> and <b>higher</b> glucose level can cause <b>hyperglycemia</b>.",
+  image: ""
+},
+5: {
+  text: "Thyroid is the largest endocrine gland which is located in neck region. It is in the shape of butterfly. Thyroid gland produce thyroxine hormone and deficiency of this hormone can cause hasimoto disease. Goitre is caused due to the deficiency of iodine. <b>Endocrine glands</b> are also called <b>ductless glands</b>.",
+  image: ""
+},
+6: {
+  text: "Brain takes upto 20% of <b>oxygen and glucose</b> that is consumed by our body. Brain consists of 60% fat. It is protected by skull which have <b>22 bones</b> (14 facial+8 cranial). Brain weighs around 1300gm in females and 1400gm in males. Between <b>meningis</b> (a protective layer) and skull <b>duramatter</b> is present.",
+  image: ""
+},
+7: {
+  text: "As cerebellum maintains the balance and posture of body. Walking in a straight line requires balance. <b>Pons</b>: It is also a part of hind brain along with <b>medulla oblongata and cerebellum</b>. It acts as a bridge between brain and spinal cord. It also regulates sleep, breathing and facial movements.",
+  image: ""
+},
+8: {
+  text: "Forebrain is divided into <b>a) Cerebrum b) Thalamus and c) Hypothalamus</b>. Hypothalamus mainly controls hunger. It also regulates body temperature. Thalamus is responsible for pain and other sensations related to taste, touch and smell.",
+  image: ""
+},
+9: {
+  text: "Dementia is a bigger term that includes many symptoms of brain dysfunction. Alzheimer's disease is a disease that leads to memory loss and other functional difficulties. It is caused by damage in nerve cells of brain. It is irreversible. <b>Amnesia</b> is usually a temporary loss of memory.",
+  image: ""
+},
+10: {
+  text: "According to this photograph, neuron is divided into 4 parts <b>a) Dendrites</b> - branch like structure which recieve signals from other neuron and body. <b>b) Cell body</b> - It contains nucleus and nissl Bodies which helps in protein synthesis. <b>c) Axon</b>- axon connects cell body to terminal. It contains schwann cells, Myelin sheath and node of ranvier( gap between myelin sheath). <b>d) Axon terminal</b> - it's the ending of a neuron where electrical signals converts into chemical to reach another neuron through synapse (gap between 2 neurons).",
+  image: ""
+},
+11: {
+  text: "The reaction used to describe structure of neuron by staining is called <b>black reaction or Golgi staining</b>. It uses pottasium dichromate (to harden) and silver nitrate. Camillo Golgi also discovered <b>Golgi apparatus in 1898</b>.",
+  image: ""
+},
+12: {
+  text: "Brain is a part of Central nervous system along with spinal cord. It is divided into 3 parts: A. <b>Forebrain</b>- control voluntary actions. It's further divided into cerebrum, thalamus and hypothalamus. B. <b>Midbrain</b>-regulate eye movement, pupil dilation and transmit information for vision and hearing. C. <b>Hind brain</b> - Cerebellum, pons and medulla Oblongata.",
+  image: ""
+},
+13: {
+  text: "Hind brain consists of three parts <b>a) cerebellum</b>: controls posture and coordination <b>b) pons</b>- connects brain and spinal cord <b>c) Medulla Oblongata</b>: part of ANS and regulated respiration, digestion and involuntary functions.<br>Temporal lobe is a part of Cerebrum. <b>Hippocampus</b> is a part of temporal lobe that processes memory. <b>Occipital lobe</b>: It is responsible for vision. <b>Parietal lobe</b>: It is related to body temperature, touch and movement. <b>Frontal lobe</b>: It's the largest lobe of cerebrum and contains motor controlling centre.",
+  image: ""
+},
+14: {
+  text: "Reflex arc is an involuntary sequence of action that protects body from damage by sensing stimuli. <b>Receptor</b> is an organ by which stimuli is sensed. Then the information is send to <b>spinal cord</b> by sensory or <b>afferent neuron</b>. Spinal cord carries the information to <b>brain</b> and command from brain to neuron and motor neuron transfer the command to the organ by which stimulai is sensed. It is a very rapid and instant process.",
+  image: ""
+},
+15: {
+  text: "Neuron is the longest cell in the body and it sense stimuli and send impulses across the body through electrical signals. These electrical signals are converted into chemical at the end of neuron and transferred to other neuron through synapse. Neurons are protected by glial cells. Schwann cells are also a type of glial cells.",
+  image: ""
+},
+16: {
+  text: "Neurons carries the information from one to another then spinal cord and brain by electrical impulses. These electrical or nerve impulse is excited or triggered by the sodium and potassium pump and makes it to travel from one neuron to other.",
+  image: ""
+}
+
+}
