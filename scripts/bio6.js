1e6fbb5b7f3d1333e228466b8ce8676e1f7900b4
Sudhanshu09102000 <127600086+Sudhanshu09102000@users.noreply.github.com>
2025-12-20 02:17:04 +0530

    Add files via upload

---
+++ scripts/bio6.js
@@ -0,0 +1,220 @@
+const questions={
+  // Start as numbering 1
+1: {
+  text: "Insufficient levels of parathyroid hormone and low amount of calcium in the blood causes which of the following conditions? [SSC MTS 04/09/2023]",
+  options: ["Malaria", "Anaemia", "Diabetes", "Hypocalcemia"],
+  correct: "4",
+  image: ""
+},
+2: {
+  text: "Which gland produces Cortisol often known as a 'stress hormone' in humans? [SSC MTS 26/10/2021]",
+  options: ["Thyroid gland", "Pituitary gland", "Adrenal gland", "Pineal gland"],
+  correct: "3",
+  image: ""
+},
+3: {
+  text: "Which of the following hormone is secreted by the brain that helps to regulate sleep-wake cycles? [SSC CHSL 05/08/2021]",
+  options: ["Insulin", "Melatonin", "Oxytocin", "Aldosterone"],
+  correct: "2",
+  image: ""
+},
+4: {
+  text: "________ is associated with the production and metabolism of fats and steroid hormones. [SSC PHASE XII 26/06/2024]",
+  options: ["Smooth endoplasmic reticulum", "Mitochondrion", "Golgi apparatus", "Nucleus"],
+  correct: "1",
+  image: ""
+},
+5: {
+  text: "'Somatotropin' is another name for ________. [SSC CHSL 05/07/2019]",
+  options: ["Digestive system", "Laughing gas", "Tear gas", "Growth hormone"],
+  correct: "4",
+  image: ""
+},
+6: {
+  text: "Which among the following mineral is essential for the synthesis of thyroid hormones (thyroxine)? [SSC MTS 13/06/2023]",
+  options: ["Iron", "Iodine", "Selenium", "Chromium"],
+  correct: "2",
+  image: ""
+},
+7: {
+  text: "Which of the following hormones is known to be helpful in coping with stress? [SSC CHSL 11/07/2019]",
+  options: ["Serotonin", "Testosterone", "Calcitonin", "Melatonin"],
+  correct: "1",
+  image: ""
+},
+8: {
+  text: "Which hormone leads to the expulsion of milk from the breast during sucking? [SSC CGL 04/06/2019]",
+  options: ["Estrogen", "Oxytocin", "Prolactin", "Progesterone"],
+  correct: "2",
+  image: ""
+},
+9: {
+  text: "Which of the following is not a plant hormone? [RRB NTPC 07/03/2021]",
+  options: ["Ethylene", "Abscisic Acid", "Gibberellins", "Prolactin"],
+  correct: "4",
+  image: ""
+},
+10: {
+  text: "Glucagon, a peptide hormone, is produced by the ________. [SSC CGL 20/08/2021]",
+  options: ["Pancreas", "Hypothalamus", "Adrenals", "Pituitary glands"],
+  correct: "1",
+  image: ""
+},
+11: {
+  text: "The atrial wall of the human heart secretes a very important peptide hormone. What is the name of that hormone? [SSC CHSL 12/10/2020]",
+  options: ["ADH", "CCK", "ANF", "GIP"],
+  correct: "3",
+  image: ""
+},
+12: {
+  text: "Which of the following is termed as a hunger hormone as its stimulates appetite, increases food intake, and promotes fat storage? [RRB NTPC CBT-II 12/06/2022]",
+  options: ["Insulin", "Resistin", "Leptin", "Ghrelin"],
+  correct: "4",
+  image: ""
+},
+13: {
+  text: "Growth hormone function: [RRB ALP 17/08/2018]",
+  options: ["Always as growth inhibitors", "Always as growth promotors", "Rarely as growth promotors", "Sometimes as growth promotors and sometimes as growth inhibitors"],
+  correct: "4",
+  image: ""
+},
+14: {
+  text: "Human growth hormone is secreted by which gland? [RRB NTPC 19/01/2021]",
+  options: ["Thyroid gland", "Anterior lobe of pituitary gland", "Pancreas", "Posterior lobe of pituitary gland"],
+  correct: "2",
+  image: ""
+},
+15: {
+  text: "Phytohormones are: [RRB ALP 20/08/2018]",
+  options: ["Regulators synthesized by plants influencing physiological processes", "Used to measure growth in plants", "Chemicals used for peeling", "Substances controlling diseases"],
+  correct: "1",
+  image: ""
+},
+16: {
+  text: "Plants require nutrients for their growth which can be supplied through ________. [SSC MTS 15/05/2023]",
+  options: ["Soil and air only", "Soil, water and air", "Soil and water only", "Soil only"],
+  correct: "2",
+  image: ""
+},
+17: {
+  text: "Water comes out of the leaves in the form of vapour by a process called ________. [SSC MTS 17/05/2023]",
+  options: ["Creepers", "Transportation", "Venation", "Transpiration"],
+  correct: "4",
+  image: ""
+},
+18: {
+  text: "What happens to the guard cells when water flows into them? [SSC CGL 27/07/2023]",
+  options: ["The guard cells close", "Stomatal pores open", "Stomatal pores shrink", "Stomatal pores close"],
+  correct: "2",
+  image: ""
+},
+19: {
+  text: "Which of the following plant hormones regulates growth, particularly by stimulating cell elongation in stems? [SSC CGL 23/08/2021]",
+  options: ["Auxin", "Ethylene", "Cytokinin", "Gibberellin"],
+  correct: "1",
+  image: ""
+},
+20: {
+  text: "Plants that are adapted to live under plenty of sunlight are called: [SSC PHASE XI 28/06/2023]",
+  options: ["Xerophytes", "Heliophytes", "Neophytes", "Protophytes"],
+  correct: "2",
+  image: ""
+},
+21: {
+  text: "The leaves have a green pigment called ________, which helps leaves to capture the energy from the sunlight. [SSC MTS 02/05/2023]",
+  options: ["Cladophyll", "Chlorophyll", "Xanthophyll", "Cytoplasm"],
+  correct: "2",
+  image: ""
+},
+22: {
+  text: "The broad, green part of the leaf is called ________. [SSC MTS 17/05/2023]",
+  options: ["Lamina", "Petiole", "Veins", "Midrib"],
+  correct: "1",
+  image: ""
+},
+23: {
+  text: "Which of the following does NOT figure among the main functions of the root system of a plant? [SSC CGL 17/08/2021]",
+  options: ["Supply of food to plant parts", "Providing a proper anchorage to the plant parts", "Absorption of water from the soil", "Absorption of minerals from the soil"],
+  correct: "1",
+  image: ""
+},
+24: {
+  text: "Which of the following gases is released in the process of photosynthesis? [SSC CHSL 12/08/2021]",
+  options: ["Nitrous dioxide", "Carbon dioxide", "Oxygen", "Nitrogen"],
+  correct: "3",
+  image: ""
+},
+25: {
+  text: "The loss of water in the form of water droplets from leaves of plants is called ________. [SSC CHSL 02/07/2024]",
+  options: ["Pressure gradient", "Plasmolysis", "Guttation", "Translocation"],
+  correct: "3",
+  image: ""
+},
+26: {
+  text: "Which one of the following pairs is not correctly matched? [CAPF 2021]",
+  options: ["Positively phototropic : Shoot", "Positively geotropic : Root", "Chemotropic : Pollen tube", "Hydrotropic : Flower"],
+  correct: "4",
+  image: ""
+},
+27: {
+  text: "A student was doing an experiment on increasing the cell division among plants. She asked her supervisor to suggest the specific plant hormone for the same. Had you been her supervisor, which plant hormone would you suggest? [CDS I 2021]",
+  options: ["Abscisic acid", "Gibberellins", "Cytokinin", "Auxin"],
+  correct: "3",
+  image: ""
+},
+28: {
+  text: "Which one of the following is essential for thyroid gland to make thyroxin? [CDS I 2023]",
+  options: ["NaCl", "KCl", "Cholesterol", "Iodine"],
+  correct: "4",
+  image: ""
+},
+29: {
+  text: "Which one among the following hormones inhibits the growth activity in plants? [CDS II 2024]",
+  options: ["Auxins", "Cytokinins", "Abscisic acid", "Gibberellins"],
+  correct: "3",
+  image: ""
+},
+30: {
+  text: "Which one of the following is a plant hormone? [CISF AC 2021]",
+  options: ["Oestrogen", "Cytokinin", "Insulin", "Thyroxin"],
+  correct: "2",
+  image: ""
+},
+31: {
+  text: "After few days of sowing of maize seeds in the soil, irrespective of their position, the radicle always grows downwards, whereas the coleoptile always grows upwards. This phenomenon is known as: [GEOSCIENTIST 2024]",
+  options: ["Phototropism", "Hydrotropism", "Chemotropism", "Geotropism"],
+  correct: "4",
+  image: ""
+},
+32: {
+  text: "Shoots of plant show upward movement and it can be designated to be: [NDA II 2021]",
+  options: ["Negatively phototropic", "Positively chemotropic", "Positively hydrotropic", "Negatively geotropic"],
+  correct: "4",
+  image: ""
+},
+33: {
+  text: "Which one of the following hormones is responsible for the development of female sexual characters? [NDA II 2021]",
+  options: ["Prolactin", "Estrogen", "Oxytocin", "Progesterone"],
+  correct: "2",
+  image: ""
+},
+34: {
+  text: "Which one of the following hormones increases the heartbeat in mammals? [NDA II 2022]",
+  options: ["Insulin", "Melatonin", "Thyroxine", "Adrenaline"],
+  correct: "4",
+  image: ""
+},
+35: {
+  text: "Xerophytic plants adapt to hot arid regions by [GEOSCIENTIST 2025]",
+  options: ["developing a thick cuticle on their leaf surfaces.", "opening stomata only during the day.", "having photosynthetic roots.", "growing leaves only during the winter."],
+  correct: "1",
+  image: ""
+},
+36: {
+  text: "Which one among the following is not a plant hormone? [CDS I 2025]",
+  options: ["Gibberellins", "Abscisic acid", "Auxins", "Thyroxine"],
+  correct: "4",
+  image: ""
+}
+
+}
---
+++ scripts/bio6exp.js
@@ -0,0 +1,148 @@
+const explanations={
+  // Start as numbering 1
+1: {
+  text: "<b>Parathyroid hormone (PTH)</b> is secreted by parathyroid gland present near thyroid glands in the neck region. It controls the <b>calcium level</b> in the blood. PTH is released when calcium level is low and it make bones release more calcium. Calcium is important for muscular contraction and nerve activities.",
+  image: ""
+},
+2: {
+  text: "<b>Adrenal gland</b> is located on the top of kidneys and it also release adernaline, aldosterone. <b>Cortisol</b> regulates blood pressure, blood sugar and stress. Low-level of cortisol can cause <b>addison's disease</b>. Adernaline is also called \"fight or flight hormone\".",
+  image: ""
+},
+3: {
+  text: "It is secreted by <b>pineal gland</b> located in brain. It also regulates circadian rhythm (body's internal clock). <b>Melatonin</b> is secreted in response to darkness which is detected by retina of our eye. More darkness leads to more melatonin and leads to a better sleep cycle.",
+  image: ""
+},
+4: {
+  text: "<b>Smooth endoplasmic reticulum</b> lacks ribosomes and mainly involved in the synthesis of lipids and fats. They also perform detoxification of poison and drugs. They are made up of large network of tubules and sheets.",
+  image: ""
+},
+5: {
+  text: "<b>Growth hormone</b> is secreted by pituitary gland/ master gland. Deficiency of growth hormone can cause <b>Dwarfism</b> and surplus can cause <b>Acromegaly</b>. Pituitary gland also secrete Anti-diuretic hormone or vasopressin that helps kidney to control the amount of water.",
+  image: ""
+},
+6: {
+  text: "<b>Thyroid</b> is largest endocrine/ductless gland. It is Butterfly shaped and located in neck region. <b>Iodine</b> is essential for secretion of thyroxine and deficiency of iodine can cause \"Goitre\" disease and Cretinism.",
+  image: ""
+},
+7: {
+  text: "<b>Serotonin hormone</b> controls mood, behaviour and acts as a messenger between body and brain. It is secreted by <b>pineal gland</b> in the brain. Pineal gland also secrete melatonin. Serotonin also controls gastrointestinal issues. Lack of serotonin can lead to depression, anxiety and other mental disorders.",
+  image: ""
+},
+8: {
+  text: "<b>Oxytocin</b> is secreted by pituitary gland and produced by hypothalamus. <b>Prolactin</b> is also secreted by pituitary gland but it is involved in the production of milk while oxytocin helps to release milk. Progesterone is involved in menstrual cycle and pregnancy.",
+  image: ""
+},
+9: {
+  text: "<b>Prolactin</b> is a hormones secreted by pituitary gland in humans after pregnancy for the formation of milk. While Ethylene, Abscisic acid and Gibberellins are plant hormone involved in growth promotion and inhibition.",
+  image: ""
+},
+10: {
+  text: "<b>Pancreas</b> is called mixed gland as it performs as both endocrine and exocrine gland, produce both hormones and enzymes. Glucagon is secreted by <b>alpha cells</b> while insulin is secreted by <b>beta cells</b> of pancreas. They both controls level of sugar in bloodstream. Increased level of glucose can cause hyperglycemia and decreased levels can cause hypoglycemia.",
+  image: ""
+},
+11: {
+  text: "<b>ANF: Atrial Natriuretic Factor</b>. Discovered by De Bold in 1981. It decreases blood pressure by decreasing water, sodium and fat levels that cause dilation of blood vessels. ADH- Anti-diuretic hormone, CCK-Cholecystokinin, GIP-gastric inhibitory polypeptide.",
+  image: ""
+},
+12: {
+  text: "It works with hypothalamus to regulate hunger. <b>Leptin:</b> It is secreted in intestine to inhibit hunger and controls metabolism. <b>Resistin</b> in produced by adipose tissue.",
+  image: ""
+},
+13: {
+  text: "<b>Growth hormone</b> is secreted by anterior lobe of Pituitary gland which is also called master gland. It can promote or inhibit the growth. Other name for growth hormone is Somatotropin. It regulates height, bone length and muscle growth. Excess level of growth hormone can cause <b>Acromegaly</b> and decreased level of growth hormone can cause dwarfism.",
+  image: ""
+},
+14: {
+  text: "Refer question 13.",
+  image: ""
+},
+15: {
+  text: "<b>Phytohormones</b> another name of plant hormones. The regulate the growth and development in plants. They can act as promoter as well as inhibitors based on their specific function.",
+  image: ""
+},
+16: {
+  text: "Soil provides essential micro and macronutrients specially Nitrogen Phosphorus and Potassium (NPK). By soil, plant also absorb the fertilizers supplements added synthetically. Some nutrients are water soluble hence absorbed by plant through water. Air provides carbon dioxide, taken by stomata which is essential for photosynthesis.",
+  image: ""
+},
+17: {
+  text: "<b>Transpiration</b> is the loss of water in form of water vapour through stomata at the daytime. It helps the plant to cool down and regulate temperature. <b>Transportation</b> is the movement of water and nutrients throughout the plant. <b>Venation</b> is pattern of veins in the plant leaf it can be parallel or branched. <b>Creepers</b> grows through adventitious roots, horizontally.",
+  image: ""
+},
+18: {
+  text: "<b>Guard cells</b> are the protective cells around stomata. The help in opening and closing of stomata and regulate transpiration. When water flows into the guard cell they cause the stomata to open and when water flows out of guard cells it closes the stomata.",
+  image: ""
+},
+19: {
+  text: "<b>Auxin</b> is a hormone that promotes growth of plant. It stimulates cell elongation in stem. It grows the tips of shoots and involve in phototropism. <b>Ethylene:</b> It is a gaseous hormone that helps in fruit ripening. It is a synthetically used in agriculture.",
+  image: ""
+},
+20: {
+  text: "<b>Heliophytes</b> need a large amount of sunlight for their optimum growth. They have thick leaves and special pigments to protect them from negative effects of sunlight. E.g. cactus, sunflowers, sugarcane.<br><b>Neophytes:</b> introduced by humans. <b>Protophytes:</b> lower class plants such as algae and fungi.",
+  image: ""
+},
+21: {
+  text: "<b>Chlorophyll</b> is present in chloroplast a type of plastid and carry out photosynthesis. <b>Xanthophyll</b> is a part of cartenoid group. It gives yellow colour to plants and protects them from intense sunlight. E.g. papaya, pumpkin and corn. <b>Cladophyll</b> is a flattened leaf or petiole. It performs photosynthesis in plants like cacti.",
+  image: ""
+},
+22: {
+  text: "<b>Lamina</b> is also called leaf blade. Chlorophyll is present in the lamina of the leaf hence photosynthesis occurs here. <b>Petiole</b> is a long and cylindrical stalk. <b>Midrib</b> is thick vein in the middle of leaf and small veins emerge from it in a definite pattern called venation.",
+  image: ""
+},
+23: {
+  text: "Root does not supply food to plant parts as photosynthesis occurs in leaves and the food is supplied throughout the plant by phloem. Root absorb water from soil and supply throughout the plant by xylem. Along with water, minerals are also absorbed by roots from the ground.",
+  image: ""
+},
+24: {
+  text: "<b>Photosynthesis</b> is the process by which plants use water and carbon dioxide in presence of light to make energy and release oxygen. Light is absorbed by chlorophyll pigment. The energy/sugar is stored in form of starch in plants.",
+  image: ""
+},
+25: {
+  text: "<b>Guttation</b> occurs through hydathodes at night while transpiration occurs through stomata at day time. <b>Translocation</b> is the movement of nutrients throughout the plant by phloem. <b>Plasmolysis</b> is the process where cells lose water in hypertonic solution to attain equilibrium. Pressure gradient enables the movement of water and nutrient in plant tissue.",
+  image: ""
+},
+26: {
+  text: "<b>Hydrotropism</b> is the movement of a plant part towards water. Flower grows against the stimulus of water hence water is negatively hydrotropic. <b>Chemotropism</b> is movement of plant part towards chemical stimulus. Pollen tube is directed towards ovary by a chemical interaction. Pollen tube is positively chemotropic.",
+  image: ""
+},
+27: {
+  text: "<b>Cytokinin</b> is a growth promoter hormone which is involved in cell division and branching. They are generally found in roots and shoots of plant. They also effect axillary bud growth and apical dominance. <b>Gibberellins:</b> It promotes fruit elongation, germination and flowering. It is present in stem.",
+  image: ""
+},
+28: {
+  text: "Refer question 6 of Level I.",
+  image: ""
+},
+29: {
+  text: "Among all the plant hormones, <b>abscisic acid inhibits</b> the growth activity. It also helps the plant to manage external stress. It is also known as <b>stress hormone</b> of plants. It regulates seed and bud dormancy and helps the plant to survive in unfavourable conditions.",
+  image: ""
+},
+30: {
+  text: "<b>Oestrogen</b> is a female hormone secreted by ovaries. Insulin is secreted by pancreas to control blood sugar levels. <b>Thyroxine</b> is secreted by thyroid gland. They all are present in humans while <b>cytokinin</b> is a plant hormone that promotes cell division in plants and present in roots and shoots.",
+  image: ""
+},
+31: {
+  text: "<b>Geotropism</b> is movement of plant part in response to gravity. The downward growth of radicle is positive geotropism while the upward growth of coleoptile indicates negative geotropism.",
+  image: ""
+},
+32: {
+  text: "Refer question 6.",
+  image: ""
+},
+33: {
+  text: "<b>Estrogen</b> is secreted by ovaries and it maintains female sexual characters. It is also known as \"beauty hormone\". <b>Prolactin</b> is secreted by pituitary gland and it enables the production of milk. <b>Progesterone</b> is involved in childbirth and pregnancy. <b>Oxytocin</b> is also secreted by pituitary gland. Also known as Love hormone.",
+  image: ""
+},
+34: {
+  text: "<b>Adrenaline</b> also called <b>fight or flight hormone</b> is secreted by adrenal glands and controls blood pressure, heartbeat and response to stress. It prepares the body to face stress and crisis. <b>Melatonin</b> is secreted by pineal gland in response to darkness.",
+  image: ""
+},
+35: {
+  text: "<b>Xerophytic plants</b> are found in dry regions like deserts. They are adapted to less water conditions such as low rainfall areas. They develop a <b>thick cuticle</b> on the leaf surface to minimise water loss through transpiration. They have Sunken stomata and deep roots. They store water in their tissues. E.g.Acacia, cactus.",
+  image: ""
+},
+36: {
+  text: "'Hormones' are named by <b>EH Starling</b>. Thyroxine is a hormone secreted by thyroid gland that is present in the neck region. It is a butterfly shaped gland and the <b>largest endocrine/ductless gland</b>. Auxins and gibberellins are growth hormone of the plant and stimulate growth of tips and elongation of fruit respectively. <b>Abscisic acid</b> is a growth inhibitor in plants.",
+  image: ""
+}
+
+}
