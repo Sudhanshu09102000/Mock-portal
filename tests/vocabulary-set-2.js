64e289f6f94f9df3faacbfd5910587c8f2acd652
Sudhanshu09102000 <127600086+Sudhanshu09102000@users.noreply.github.com>
2025-12-29 17:32:10 +0530

    Update vocabulary-set-2.js

--- tests/vocabulary-set-2.js
+++ tests/vocabulary-set-2.js
@@ -357,252 +357,253 @@ const explanations = {
 
 1:{
 text:"<b><u>उच्च प्रशंसा करना</u></b><br>Exalt means to praise someone greatly or raise them to a higher status.<br><b>Synonym:</b> praise<br><b>Antonym:</b> demean",
-image:"/images/vocabulary-set-2/1to5.png"
+image:"images/vocabulary-set-2/1to5.png"
 },
 
 2:{
 text:"<b><u>स्मरण में सम्मान करना</u></b><br>Commemorate means to honour or remember an important person or event.<br><b>Synonym:</b> honor<br><b>Antonym:</b> forget",
-image:"/images/vocabulary-set-2/1to5.png"
+image:"images/vocabulary-set-2/1to5.png"
 },
 
 3:{
 text:"<b><u>पूरी तरह नष्ट होना</u></b><br>Wrecked means badly damaged or destroyed completely.<br><b>Synonym:</b> destroyed<br><b>Antonym:</b> repaired",
-image:"/images/vocabulary-set-2/1to5.png"
+image:"images/vocabulary-set-2/1to5.png"
 },
 
 4:{
 text:"<b><u>विनम्र और आज्ञाकारी</u></b><br>Meek refers to someone who is gentle and submissive by nature.<br><b>Synonym:</b> submissive<br><b>Antonym:</b> aggressive",
-image:"/images/vocabulary-set-2/1to5.png"
+image:"images/vocabulary-set-2/1to5.png"
 },
 
 5:{
 text:"<b><u>घेराबंदी करना</u></b><br>Besieged means surrounded by enemy forces to force surrender.<br><b>Synonym:</b> surrounded<br><b>Antonym:</b> freed",
-image:"/images/vocabulary-set-2/1to5.png"
+image:"images/vocabulary-set-2/1to5.png"
 },
 
 6:{
 text:"<b><u>किले की रक्षा दीवार</u></b><br>Rampart is a strong defensive wall of a fort or city.<br><b>Synonym:</b> fortification<br><b>Antonym:</b> opening",
-image:"/images/vocabulary-set-2/6to10.png"
+image:"images/vocabulary-set-2/6to10.png"
 },
 
 7:{
 text:"<b><u>जीवों का वर्गीकरण करने वाला</u></b><br>A taxonomist classifies plants and animals into categories.<br><b>Synonym:</b> classifier<br><b>Antonym:</b> generalist",
-image:"/images/vocabulary-set-2/6to10.png"
+image:"images/vocabulary-set-2/6to10.png"
 },
 
 8:{
 text:"<b><u>शक्ति की कमी</u></b><br>Enervation means physical or mental exhaustion and loss of strength.<br><b>Synonym:</b> exhaustion<br><b>Antonym:</b> vitality",
-image:"/images/vocabulary-set-2/6to10.png"
+image:"images/vocabulary-set-2/6to10.png"
 },
 
 9:{
 text:"<b><u>जानबूझकर दूर रहना</u></b><br>Abstention is the act of deliberately not taking part.<br><b>Synonym:</b> avoidance<br><b>Antonym:</b> participation",
-image:"/images/vocabulary-set-2/6to10.png"
+image:"images/vocabulary-set-2/6to10.png"
 },
 
 10:{
 text:"<b><u>छुपाना</u></b><br>Conceal means to hide something from view or knowledge.<br><b>Synonym:</b> hide<br><b>Antonym:</b> reveal",
-image:"/images/vocabulary-set-2/6to10.png"
+image:"images/vocabulary-set-2/6to10.png"
 },
 
 11:{
 text:"<b><u>बाहर निकालना</u></b><br>Elicit means to draw out information or response.<br><b>Synonym:</b> extract<br><b>Antonym:</b> suppress",
-image:"/images/vocabulary-set-2/11to15.png"
+image:"images/vocabulary-set-2/11to15.png"
 },
 
 12:{
 text:"<b><u>रुकना</u></b><br>Halt means to stop movement or action.<br><b>Synonym:</b> stop<br><b>Antonym:</b> proceed",
-image:"/images/vocabulary-set-2/11to15.png"
+image:"images/vocabulary-set-2/11to15.png"
 },
 
 13:{
 text:"<b><u>सूचित करना</u></b><br>Apprise means to inform someone officially.<br><b>Synonym:</b> inform<br><b>Antonym:</b> mislead",
-image:"/images/vocabulary-set-2/11to15.png"
+image:"images/vocabulary-set-2/11to15.png"
 },
 
 14:{
 text:"<b><u>अत्यंत शानदार</u></b><br>Stupendous means extremely impressive or remarkable.<br><b>Synonym:</b> magnificent<br><b>Antonym:</b> ordinary",
-image:"/images/vocabulary-set-2/11to15.png"
+image:"images/vocabulary-set-2/11to15.png"
 },
 
 15:{
 text:"<b><u>बाढ़</u></b><br>Deluge means a sudden flood or overwhelming quantity.<br><b>Synonym:</b> flood<br><b>Antonym:</b> drought",
-image:"/images/vocabulary-set-2/11to15.png"
+image:"images/vocabulary-set-2/11to15.png"
 },
 
 16:{
 text:"<b><u>व्यापक</u></b><br>Sweeping means wide-ranging or extensive.<br><b>Synonym:</b> extensive<br><b>Antonym:</b> limited",
-image:"/images/vocabulary-set-2/16to20.png"
+image:"images/vocabulary-set-2/16to20.png"
 },
 
 17:{
 text:"<b><u>विनम्रता से</u></b><br>Meekly means in a submissive or gentle manner.<br><b>Synonym:</b> humbly<br><b>Antonym:</b> boldly",
-image:"/images/vocabulary-set-2/16to20.png"
+image:"images/vocabulary-set-2/16to20.png"
 },
 
 18:{
 text:"<b><u>कठोर</u></b><br>Stern means strict and serious in attitude.<br><b>Synonym:</b> strict<br><b>Antonym:</b> lenient",
-image:"/images/vocabulary-set-2/16to20.png"
+image:"images/vocabulary-set-2/16to20.png"
 },
 
 19:{
 text:"<b><u>मूर्खतापूर्ण</u></b><br>Fatuous means silly or lacking serious thought.<br><b>Synonym:</b> foolish<br><b>Antonym:</b> sensible",
-image:"/images/vocabulary-set-2/16to20.png"
+image:"images/vocabulary-set-2/16to20.png"
 },
 
 20:{
 text:"<b><u>परिवर्तनशील</u></b><br>Mercurial means changeable or unpredictable in nature.<br><b>Synonym:</b> volatile<br><b>Antonym:</b> stable",
-image:"/images/vocabulary-set-2/16to20.png"
+image:"images/vocabulary-set-2/16to20.png"
 },
 
 21:{
 text:"<b><u>लापरवाह और असम्मानजनक</u></b><br>Flippant means showing casual disrespect or lack of seriousness.<br><b>Synonym:</b> casual<br><b>Antonym:</b> serious",
-image:"/images/vocabulary-set-2/21to25.png"
+image:"images/vocabulary-set-2/21to25.png"
 },
 
 22:{
 text:"<b><u>शांत</u></b><br>Tranquil means calm, peaceful, and undisturbed.<br><b>Synonym:</b> calm<br><b>Antonym:</b> agitated",
-image:"/images/vocabulary-set-2/21to25.png"
+image:"images/vocabulary-set-2/21to25.png"
 },
 
 23:{
 text:"<b><u>कसकर बंद</u></b><br>Clenched means tightly closed, often due to anger or tension.<br><b>Synonym:</b> tightened<br><b>Antonym:</b> relaxed",
-image:"/images/vocabulary-set-2/21to25.png"
+image:"images/vocabulary-set-2/21to25.png"
 },
 
 24:{
 text:"<b><u>मजबूती से पकड़ना</u></b><br>Clasped means held tightly with hands or arms.<br><b>Synonym:</b> gripped<br><b>Antonym:</b> released",
-image:"/images/vocabulary-set-2/21to25.png"
+image:"images/vocabulary-set-2/21to25.png"
 },
 
 25:{
 text:"<b><u>कंजूस व्यक्ति</u></b><br>Miser is a person who hoards money and avoids spending.<br><b>Synonym:</b> hoarder<br><b>Antonym:</b> spender",
-image:"/images/vocabulary-set-2/21to25.png"
+image:"images/vocabulary-set-2/21to25.png"
 },
 
 26:{
 text:"<b><u>अल्पता</u></b><br>Meagerness refers to scarcity or insufficiency.<br><b>Synonym:</b> scarcity<br><b>Antonym:</b> abundance",
-image:"/images/vocabulary-set-2/26to30.png"
+image:"images/vocabulary-set-2/26to30.png"
 },
 
 27:{
 text:"<b><u>उद्धार</u></b><br>Salvation means rescue or deliverance from harm or difficulty.<br><b>Synonym:</b> rescue<br><b>Antonym:</b> ruin",
-image:"/images/vocabulary-set-2/26to30.png"
+image:"images/vocabulary-set-2/26to30.png"
 },
 
 28:{
 text:"<b><u>विशाल</u></b><br>Colossal means extremely large in size or extent.<br><b>Synonym:</b> enormous<br><b>Antonym:</b> tiny",
-image:"/images/vocabulary-set-2/26to30.png"
+image:"images/vocabulary-set-2/26to30.png"
 },
 
 29:{
 text:"<b><u>स्वीकार करना</u></b><br>Accede means to agree to a request or demand formally.<br><b>Synonym:</b> consent<br><b>Antonym:</b> refuse",
-image:"/images/vocabulary-set-2/26to30.png"
+image:"images/vocabulary-set-2/26to30.png"
 },
 
 30:{
 text:"<b><u>कटु</u></b><br>Acerbic means sharp, bitter, or harsh in tone.<br><b>Synonym:</b> caustic<br><b>Antonym:</b> mild",
-image:"/images/vocabulary-set-2/26to30.png"
+image:"images/vocabulary-set-2/26to30.png"
 },
 
 31:{
 text:"<b><u>बढ़ाना</u></b><br>Aggrandize means to increase power, wealth, or importance.<br><b>Synonym:</b> enlarge<br><b>Antonym:</b> reduce",
-image:"/images/vocabulary-set-2/31to35.png"
+image:"images/vocabulary-set-2/31to35.png"
 },
 
 32:{
 text:"<b><u>गरिमा</u></b><br>Grace means elegance and dignity in behavior.<br><b>Synonym:</b> elegance<br><b>Antonym:</b> awkwardness",
-image:"/images/vocabulary-set-2/31to35.png"
+image:"images/vocabulary-set-2/31to35.png"
 },
 
 33:{
 text:"<b><u>कर्कश</u></b><br>Grating means irritating or unpleasant to senses.<br><b>Synonym:</b> harsh<br><b>Antonym:</b> pleasant",
-image:"/images/vocabulary-set-2/31to35.png"
+image:"images/vocabulary-set-2/31to35.png"
 },
 
 34:{
 text:"<b><u>मंच का अगला भाग</u></b><br>Proscenium is the front part of a theatre stage.<br><b>Synonym:</b> stage-front<br><b>Antonym:</b> backstage",
-image:"/images/vocabulary-set-2/31to35.png"
+image:"images/vocabulary-set-2/31to35.png"
 },
 
 35:{
 text:"<b><u>मानदेय</u></b><br>Honorarium is a payment given for professional services voluntarily rendered.<br><b>Synonym:</b> fee<br><b>Antonym:</b> penalty",
-image:"/images/vocabulary-set-2/31to35.png"
+image:"images/vocabulary-set-2/31to35.png"
 },
 
 36:{
 text:"<b><u>अव्यवस्था</u></b><br>Pandemonium means chaos or wild disorder.<br><b>Synonym:</b> chaos<br><b>Antonym:</b> order",
-image:"/images/vocabulary-set-2/36to40.png"
+image:"images/vocabulary-set-2/36to40.png"
 },
 
 37:{
 text:"<b><u>निंदा</u></b><br>Opprobrium means public disgrace or harsh criticism.<br><b>Synonym:</b> disgrace<br><b>Antonym:</b> praise",
-image:"/images/vocabulary-set-2/36to40.png"
+image:"images/vocabulary-set-2/36to40.png"
 },
 
 38:{
 text:"<b><u>मीनार</u></b><br>Minaret is a tall slender tower of a mosque.<br><b>Synonym:</b> tower<br><b>Antonym:</b> basement",
-image:"/images/vocabulary-set-2/36to40.png"
+image:"images/vocabulary-set-2/36to40.png"
 },
 
 39:{
 text:"<b><u>बिना वसीयत के</u></b><br>Intestate means dying without making a legal will.<br><b>Synonym:</b> unwilled<br><b>Antonym:</b> testate",
-image:"/images/vocabulary-set-2/36to40.png"
+image:"images/vocabulary-set-2/36to40.png"
 },
 
 40:{
 text:"<b><u>अहंकार</u></b><br>Conceit means excessive pride in oneself.<br><b>Synonym:</b> vanity<br><b>Antonym:</b> humility",
-image:"/images/vocabulary-set-2/36to40.png"
+image:"images/vocabulary-set-2/36to40.png"
 },
 
 41:{
 text:"<b><u>डांट भरा भाषण</u></b><br>Harangue means a long, forceful speech.<br><b>Synonym:</b> tirade<br><b>Antonym:</b> praise",
-image:"/images/vocabulary-set-2/41to45.png"
+image:"images/vocabulary-set-2/41to45.png"
 },
 
 42:{
 text:"<b><u>धोखा</u></b><br>Hoax means a false trick or deception.<br><b>Synonym:</b> fraud<br><b>Antonym:</b> truth",
-image:"/images/vocabulary-set-2/41to45.png"
+image:"images/vocabulary-set-2/41to45.png"
 },
 
 43:{
 text:"<b><u>बिखरा हुआ</u></b><br>Scruffy means untidy or shabby in appearance.<br><b>Synonym:</b> shabby<br><b>Antonym:</b> neat",
-image:"/images/vocabulary-set-2/41to45.png"
+image:"images/vocabulary-set-2/41to45.png"
 },
 
 44:{
 text:"<b><u>सामान्य</u></b><br>Generic refers to something general and not branded.<br><b>Synonym:</b> common<br><b>Antonym:</b> specific",
-image:"/images/vocabulary-set-2/41to45.png"
+image:"images/vocabulary-set-2/41to45.png"
 },
 
 45:{
 text:"<b><u>भड़कीला</u></b><br>Gaudy means overly bright or showy.<br><b>Synonym:</b> flashy<br><b>Antonym:</b> plain",
-image:"/images/vocabulary-set-2/41to45.png"
+image:"images/vocabulary-set-2/41to45.png"
 },
 
 46:{
 text:"<b><u>प्रेरणापूर्ण आग्रह</u></b><br>Exhortation means strong urging or encouragement to act.<br><b>Synonym:</b> urging<br><b>Antonym:</b> discouragement",
-image:"/images/vocabulary-set-2/46to50.png"
+image:"images/vocabulary-set-2/46to50.png"
 },
 
 47:{
 text:"<b><u>कमज़ोर</u></b><br>Infirm means physically weak, especially due to age or illness.<br><b>Synonym:</b> frail<br><b>Antonym:</b> robust",
-image:"/images/vocabulary-set-2/46to50.png"
+image:"images/vocabulary-set-2/46to50.png"
 },
 
 48:{
 text:"<b><u>समतल तली वाली नाव</u></b><br>Barge is a flat-bottomed boat used mainly on rivers.<br><b>Synonym:</b> vessel<br><b>Antonym:</b> submarine",
-image:"/images/vocabulary-set-2/46to50.png"
+image:"images/vocabulary-set-2/46to50.png"
 },
 
 49:{
 text:"<b><u>संक्षिप्त लेकिन सारगर्भित</u></b><br>Pithy means brief yet full of meaning.<br><b>Synonym:</b> concise<br><b>Antonym:</b> verbose",
-image:"/images/vocabulary-set-2/46to50.png"
+image:"images/vocabulary-set-2/46to50.png"
 },
 
 50:{
 text:"<b><u>खुलासा</u></b><br>Divulgence means the act of revealing secret information.<br><b>Synonym:</b> disclosure<br><b>Antonym:</b> concealment",
-image:"/images/vocabulary-set-2/46to50.png"
+image:"images/vocabulary-set-2/46to50.png"
 }
 
 };
+
