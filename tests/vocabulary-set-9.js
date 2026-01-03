35e66f0b64d265921db8266e0a84ebce45b62e19
Sudhanshu09102000 <127600086+Sudhanshu09102000@users.noreply.github.com>
2026-01-04 04:20:50 +0530

    Update vocabulary-set-9.js

--- tests/vocabulary-set-9.js
+++ tests/vocabulary-set-9.js
@@ -147,6 +147,76 @@ options: ["Careful not to offend others", "Rude and blunt", "Dishonest", "Indiff
 correct: "1",
 image: ''
 },
+
+22: {
+text: "The loose wire <b>dangled</b> from the ceiling, posing a safety risk.<br>Choose the correct meaning of <b>dangled</b>.",
+options: ["Hung or swung loosely", "Fixed firmly in place", "Cut off completely", "Hidden from view"],
+correct: "1",
+image: ''
+},
+
+23: {
+text: "The pianist was hailed as a <b>virtuoso</b> after his flawless performance.<br>Choose the correct meaning of <b>virtuoso</b>.",
+options: ["A highly skilled performer", "An amateur learner", "A casual listener", "A music critic"],
+correct: "1",
+image: ''
+},
+
+24: {
+text: "She is admired for her <b>virtuous</b> conduct and honesty.<br>Choose the correct meaning of <b>virtuous</b>.",
+options: ["Morally good and righteous", "Highly talented", "Strictly traditional", "Socially popular"],
+correct: "1",
+image: ''
+},
+
+25: {
+text: "The government decided to hold a <b>referendum</b> on the proposed law.<br>Choose the correct meaning of <b>referendum</b>.",
+options: ["Direct vote by the people", "Debate in parliament", "Judicial review", "Administrative order"],
+correct: "1",
+image: ''
+},
+
+26: {
+text: "Each region has its own <b>dialect</b> with distinct expressions.<br>Choose the correct meaning of <b>dialect</b>.",
+options: ["A regional form of a language", "A foreign language", "A written script", "A technical jargon"],
+correct: "1",
+image: ''
+},
+
+27: {
+text: "His excuse for being late sounded <b>ludicrous</b> to everyone present.<br>Choose the correct meaning of <b>ludicrous</b>.",
+options: ["Absurd or ridiculous", "Highly convincing", "Formally acceptable", "Partially true"],
+correct: "1",
+image: ''
+},
+
+28: {
+text: "After the storm, the room looked completely <b>disheveled</b>.<br>Choose the correct meaning of <b>disheveled</b>.",
+options: ["Untidy and messy", "Neatly arranged", "Recently renovated", "Lightly decorated"],
+correct: "1",
+image: ''
+},
+
+29: {
+text: "The scandal <b>tainted</b> his reputation permanently.<br>Choose the correct meaning of <b>tainted</b>.",
+options: ["Spoiled or corrupted", "Greatly enhanced", "Officially approved", "Widely praised"],
+correct: "1",
+image: ''
+},
+
+30: {
+text: "She began to <b>peruse</b> the document before signing it.<br>Choose the correct meaning of <b>peruse</b>.",
+options: ["Read or examine carefully", "Ignore completely", "Summarize briefly", "Destroy intentionally"],
+correct: "1",
+image: ''
+},
+
+31: {
+text: "There was a sense of <b>anticipation</b> before the exam results were announced.<br>Choose the correct meaning of <b>anticipation</b>.",
+options: ["Expectation or excitement", "Complete surprise", "Total confusion", "Strong disappointment"],
+correct: "1",
+image: ''
+},
 }
 
 const explanations = {
@@ -254,6 +324,57 @@ image: "images/vocabulary-set-9/16to20.png"
 text: "<b><u>समझदारी से बोलने वाला</u></b><br>Tactful means speaking or behaving carefully so as not to hurt or offend others.<br><b>Synonym:</b><br>diplomatic<br><b>Antonym:</b><br>blunt",
 image: "images/vocabulary-set-9/21to25.png"
 },
+
+  22: {
+text: "<b><u>लटका हुआ</u></b><br>Dangled means hanging or swinging loosely without firm support, often moving freely in the air.<br><b>Synonym:</b><br>hanging<br><b>Antonym:</b><br>fixed",
+image: "images/vocabulary-set-9/21to25.png"
+},
+
+23: {
+text: "<b><u>अत्यंत कुशल कलाकार</u></b><br>Virtuoso refers to a person who has exceptional technical skill, especially in music or art.<br><b>Synonym:</b><br>expert<br><b>Antonym:</b><br>novice",
+image: "images/vocabulary-set-9/21to25.png"
+},
+
+24: {
+text: "<b><u>नैतिक और सदाचारी</u></b><br>Virtuous describes a person who behaves according to high moral standards and righteousness.<br><b>Synonym:</b><br>moral<br><b>Antonym:</b><br>corrupt",
+image: "images/vocabulary-set-9/21to25.png"
+},
+
+25: {
+text: "<b><u>जनमत संग्रह</u></b><br>Referendum is a direct vote in which the entire electorate is asked to decide on a specific proposal.<br><b>Synonym:</b><br>plebiscite<br><b>Antonym:</b><br>dictatorship",
+image: "images/vocabulary-set-9/21to25.png"
+},
+
+26: {
+text: "<b><u>क्षेत्रीय भाषा रूप</u></b><br>Dialect is a particular form of a language spoken in a specific region or by a particular group.<br><b>Synonym:</b><br>vernacular<br><b>Antonym:</b><br>standard language",
+image: "images/vocabulary-set-9/26to30.png"
+},
+
+27: {
+text: "<b><u>हास्यास्पद</u></b><br>Ludicrous means so unreasonable or absurd that it provokes laughter or disbelief.<br><b>Synonym:</b><br>ridiculous<br><b>Antonym:</b><br>serious",
+image: "images/vocabulary-set-9/26to30.png"
+},
+
+28: {
+text: "<b><u>अस्त-व्यस्त</u></b><br>Disheveled describes a state of being untidy, disordered, or messy in appearance.<br><b>Synonym:</b><br>messy<br><b>Antonym:</b><br>neat",
+image: "images/vocabulary-set-9/26to30.png"
+},
+
+29: {
+text: "<b><u>दूषित</u></b><br>Tainted means contaminated, spoiled, or damaged in quality or reputation.<br><b>Synonym:</b><br>corrupted<br><b>Antonym:</b><br>pure",
+image: "images/vocabulary-set-9/26to30.png"
+},
+
+30: {
+text: "<b><u>ध्यान से पढ़ना</u></b><br>Peruse means to read, examine, or study something carefully and thoroughly.<br><b>Synonym:</b><br>examine<br><b>Antonym:</b><br>ignore",
+image: "images/vocabulary-set-9/26to30.png"
+},
+
+31: {
+text: "<b><u>पूर्व-उत्सुकता</u></b><br>Anticipation refers to the feeling of excitement or expectation about something that is going to happen.<br><b>Synonym:</b><br>expectation<br><b>Antonym:</b><br>indifference",
+image: "images/vocabulary-set-9/31to35.png"
+}
 };
+
 
 
