22a5ed59dc9ea191b8a4c30928860d38f6852bbf
Sudhanshu09102000 <127600086+Sudhanshu09102000@users.noreply.github.com>
2026-03-18 21:45:53 +0530

    Add files via upload

---
+++ scripts/guest.js
@@ -0,0 +1,16 @@
+window.TEST_DATA = {
+  "1": {
+    "questext": "",
+    "options": [
+      "",
+      "",
+      "",
+      ""
+    ],
+    "correct": "1",
+    "quesimage": "",
+    "exptext": "",
+    "expimage": ""
+  },
+  
+};
