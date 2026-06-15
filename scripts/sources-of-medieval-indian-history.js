39c75b3770813cfdc7ba7d44b35f6792bf9d97c5
Sudhanshu09102000 <127600086+Sudhanshu09102000@users.noreply.github.com>
2026-06-15 16:53:37 +0530

    Create sources-of-medieval-indian-history.js

---
+++ scripts/sources-of-medieval-indian-history.js
@@ -0,0 +1,184 @@
+window.TEST_DATA = {
+  "1": {
+    "questext": "Who wrote the book 'Tarikh-ul-Hind' (also known as 'Kitab-ul-Hind')?",
+    "options": [
+      "Ibn Battuta",
+      "Alberuni",
+      "Ziauddin Barani",
+      "Minhaj-us-Siraj"
+    ],
+    "correct": "2",
+    "quesimage": "",
+    "exptext": "<b>Alberuni</b><br><br><b>Alberuni</b> wrote the book <b>'Tarikh-ul-Hind'</b> (also known as <b>'Tahqiq-e-Hind'</b> or <b>'Kitab-ul-Hind'</b>). He came to India at the time of <b>Mahmud Ghaznavi's invasion</b>. He was a scholar of <b>Arabic and Persian languages</b>. The book was later translated into English by <b>Edward Sachi</b>.",
+    "expimage": ""
+  },
+  "2": {
+    "questext": "Which book gives details of events in India from 1192 AD to 1228 AD?",
+    "options": [
+      "Tabakat-e-Nasiri",
+      "Tarikh-e-Firozshahi",
+      "Taj-ul-Masir",
+      "Chachnama"
+    ],
+    "correct": "3",
+    "quesimage": "",
+    "exptext": "<b>Taj-ul-Masir</b><br><br><b>Taj-ul-Masir</b> was written by <b>Hasan Nizami</b>. This book gives details of the events of India from <b>1192 AD to 1228 AD</b>.<br><br><b>Tarikh-e-Firozshahi</b> is the work of <b>Ziauddin Barani</b>, who was a contemporary of the <b>Tughlaq rulers</b>. Barani also composed <b>Fatwa-e-Jahandari</b>.<br><br><b>Futuhat-e-Ferozshahi</b> was written by <b>Firozshah Tughlaq</b> himself.",
+    "expimage": ""
+  },
+  "3": {
+    "questext": "Who is the author of 'Tabakat-e-Nasiri', which describes the conquest of India by Muhammad Ghori till 1259–60 AD?",
+    "options": [
+      "Hasan Nizami",
+      "Ziauddin Barani",
+      "Amir Khusro",
+      "Minhaj-us-Siraj"
+    ],
+    "correct": "4",
+    "quesimage": "",
+    "exptext": "<b>Minhaj-us-Siraj</b><br><br>The author of <b>Tabakat-e-Nasiri</b> is <b>Minhaj-us-Siraj</b>, who described the conquest of India by <b>Muhammad Ghori</b> till <b>1259–60 AD</b>.<br><br><b>Chachnama</b> was translated into <b>Persian</b> by <b>Muhammad Ali-bin-Abu Bakr Kufi</b> during the time of <b>Nasiruddin Quwacha</b>. It is the main source of information about the <b>Arab conquest of Sindh</b>.<br><br><b>Kitab-ul-Yamini</b> was composed by <b>Utbi</b>.",
+    "expimage": ""
+  },
+  "4": {
+    "questext": "Ibn Battuta, the author of 'Kitab-ul-Rehla', came to India in 1333 AD and served in the court of which ruler?",
+    "options": [
+      "Alauddin Khilji",
+      "Firoz Shah Tughlaq",
+      "Muhammad bin Tughlaq",
+      "Balban"
+    ],
+    "correct": "3",
+    "quesimage": "",
+    "exptext": "<b>Muhammad bin Tughlaq</b><br><br><b>Ibn Battuta</b>, an African traveller from <b>Morocco</b>, came to India in <b>1333 AD</b> and stayed here till <b>1342 AD</b>. He worked as a <b>judge (Qazi)</b> in the court of <b>Muhammad bin Tughlaq</b>. He wrote extensively about India in his book <b>'Kitab-ul-Rehla'</b>.",
+    "expimage": ""
+  },
+  "5": {
+    "questext": "Which of the following correctly matches a literary work with its author?",
+    "options": [
+      "Tughlaqnama — Ziauddin Barani",
+      "Rajatarangini — Chandbardai",
+      "Prithviraj Raso — Amir Khusro",
+      "Hammir Mahakavya — Nayanchandra Suri"
+    ],
+    "correct": "4",
+    "quesimage": "",
+    "exptext": "<b>Hammir Mahakavya — Nayanchandra Suri</b><br><br>The correct author–work pairs are as follows:<br><br>• <b>Tughlaqnama</b> — <b>Amir Khusro</b><br>• <b>Rajatarangini</b> — <b>Kalhan</b><br>• <b>Prithviraj Raso</b> — <b>Chandbardai</b><br>• <b>Hammir Mahakavya</b> — <b>Nayanchandra Suri</b><br><br>We get information about <b>Rana Hammir of Ranthambore</b> from <b>'Hammir Mahakavya'</b> written by <b>Nayanchandra Suri</b>.",
+    "expimage": ""
+  },
+  "6": {
+    "questext": "In which language did Babur compose his autobiography 'Tuzuk-e-Babri' (Baburnama)?",
+    "options": [
+      "Persian",
+      "Arabic",
+      "Turkish",
+      "Urdu"
+    ],
+    "correct": "3",
+    "quesimage": "",
+    "exptext": "<b>Turkish</b><br><br><b>Tuzuk-e-Babri</b> (also known as <b>Baburnama</b>) is the <b>autobiography of Babur</b>. Babur composed this work in the <b>Turkish language</b>.<br><br><b>Humayunama</b> was written in <b>Persian language</b> by Humayun's sister <b>Gulbadan Begum</b>.<br><br><b>Tarikh-e-Shershahi</b> was authored by <b>Abbas Khan Sherwani</b>.<br><br><b>Tarikh-e-Rashidi</b> was written by <b>Mirza Haider Dughlat</b>. It describes the <b>last days of Babur</b> and the events of the reigns of <b>Humayun</b> and <b>Sher Shah</b>.",
+    "expimage": ""
+  },
+  "7": {
+    "questext": "Who composed 'Akbarnama', and what is its third part called?",
+    "options": [
+      "Khwaja Nizamuddin Ahmad; Tabakat-e-Akbari",
+      "Abul Fazal; Ain-e-Akbari",
+      "Muhammad Qasim Farishta; Tarikh-e-Farishta",
+      "Amir Khusro; Tughlaqnama"
+    ],
+    "correct": "2",
+    "quesimage": "",
+    "exptext": "<b>Abul Fazal; Ain-e-Akbari</b><br><br><b>Akbarnama</b> is the work of <b>Abul Fazal</b>. Its <b>third part</b> is called <b>'Ain-e-Akbari'</b>.<br><br><b>Tabakat-e-Akbari</b> was composed by <b>Khwaja Nizamuddin Ahmad</b>.<br><br><b>Tarikh-e-Farishta</b> was composed by <b>Muhammad Qasim Farishta</b>.",
+    "expimage": ""
+  },
+  "8": {
+    "questext": "Who helped Jahangir in completing his autobiography 'Tujuk-e-Jahangiri'?",
+    "options": [
+      "Abul Fazal and Faizi",
+      "Mutamid Khan and Bakshi",
+      "Abdul Hamid Lahori and Qazvini",
+      "Inayat Khan and Khafi Khan"
+    ],
+    "correct": "2",
+    "quesimage": "",
+    "exptext": "<b>Mutamid Khan and Bakshi</b><br><br><b>Tujuk-e-Jahangiri</b> is the <b>autobiography of Jahangir</b>. <b>Mutamid Khan</b> and <b>Bakshi</b> also helped in completing this work.<br><br>The prominent literary works of <b>Shahjahan's era</b> include:<br>• <b>Padshahnama</b> — composed by <b>Qazvini</b><br>• <b>Shahjahannama</b> — composed by <b>Inayat Khan</b><br>• <b>Badshahnama</b> — composed by <b>Abdul Hamid Lahori</b>",
+    "expimage": ""
+  },
+  "9": {
+    "questext": "Which of the following works was written during the reign of Aurangzeb by Khafi Khan?",
+    "options": [
+      "Alamgirnama",
+      "Maasir-e-Alamgiri",
+      "Muntakhab-ul-Lubab",
+      "Futuhat-e-Alamgiri"
+    ],
+    "correct": "3",
+    "quesimage": "",
+    "exptext": "<b>Muntakhab-ul-Lubab</b><br><br>The following major works were written during the reign of <b>Aurangzeb</b>:<br><br>• <b>Muntakhab-ul-Lubab</b> — <b>Khafi Khan</b><br>• <b>Alamgirnama</b> — <b>Mirza Muhammad Kazim</b><br>• <b>Maasir-e-Alamgiri</b> — <b>Saqi Mustad Khan</b><br>• <b>Futuhat-e-Alamgiri</b> — <b>Ishwar Das Nagar</b><br>• <b>Nushra-e-Dilkusha</b> — <b>Bhimsen</b>",
+    "expimage": ""
+  },
+  "10": {
+    "questext": "Marco Polo, who is called the 'Prince of Medieval Travellers', visited the court of which Indian king during the 13th century?",
+    "options": [
+      "Devaraya II of Vijayanagara",
+      "Muhammad bin Tughlaq of Delhi",
+      "The Pandya king of South India",
+      "Krishnadeva Raya of Vijayanagara"
+    ],
+    "correct": "3",
+    "quesimage": "",
+    "exptext": "<b>The Pandya king of South India</b><br><br><b>Marco Polo</b> came to India in the <b>13th century</b>. He was a native of <b>Venice</b> and visited the court of the <b>Pandya king</b>. Marco Polo is called the <b>'Prince of Medieval Travellers'</b>.",
+    "expimage": ""
+  },
+  "11": {
+    "questext": "Which foreign traveller visited the Vijayanagara Empire as an ambassador of Shahrukh (ruler of the Timur dynasty) during the reign of Devaraya II?",
+    "options": [
+      "Niccolò de Conti",
+      "Abdur Razzaq",
+      "Domingo Paes",
+      "Fernao Nunez"
+    ],
+    "correct": "2",
+    "quesimage": "",
+    "exptext": "<b>Abdur Razzaq</b><br><br><b>Abdur Razzaq</b> came to India in <b>1442–43 AD</b>. He visited the <b>Vijayanagara Empire</b> as an ambassador of <b>Shahrukh</b>, the ruler of the <b>Timur dynasty</b>, during the reign of <b>Devaraya II</b>.<br><br><b>Niccolò de Conti</b> was a resident of <b>Italy</b>. He came to India in <b>1420–21 AD</b> and visited the Vijayanagara Empire during the reign of <b>Devaraya I</b>.",
+    "expimage": ""
+  },
+  "12": {
+    "questext": "Which Portuguese traveller described the historical aspects of ancient Hampi during the reign of Krishnadeva Raya of the Vijayanagara Empire?",
+    "options": [
+      "Fernao Nunez",
+      "Niccolò de Conti",
+      "Abdur Razzaq",
+      "Domingo Paes"
+    ],
+    "correct": "4",
+    "quesimage": "",
+    "exptext": "<b>Domingo Paes</b><br><br><b>Domingo Paes</b> was a <b>Portuguese traveller</b> who visited South India during the reign of <b>Krishnadeva Raya</b>, ruler of the <b>Vijayanagara Empire</b>. He described in detail all the historical aspects of ancient <b>Hampi</b>.<br><br><b>Fernao Nunez</b> was also a <b>Portuguese traveller</b>, historian, and horse trader. He visited India during the reign of <b>Achyutraya</b> and spent <b>three years</b> in Vijayanagara.",
+    "expimage": ""
+  },
+  "13": {
+    "questext": "Which of the following structures was NOT built by Qutubuddin Aibak?",
+    "options": [
+      "Quwwat-ul-Islam Mosque",
+      "Adhai Din Ka Jhonpra",
+      "Alai Darwaza",
+      "Qutub Minar"
+    ],
+    "correct": "3",
+    "quesimage": "",
+    "exptext": "<b>Alai Darwaza</b><br><br><b>Qutubuddin Aibak</b> built the following structures, which are living examples of <b>Indo-Islamic architecture</b>:<br>• <b>Quwwat-ul-Islam Mosque</b> — Mehrauli, Delhi<br>• <b>Adhai Din Ka Jhonpra</b> — Ajmer<br>• <b>Qutub Minar</b> — Mehrauli, Delhi (construction started in <b>1199 AD</b>)<br><br>The <b>Qutub Minar</b> was expanded to <b>four storeys</b> by <b>Iltutmish</b> and later repaired by <b>Firoz Shah Tughlaq</b>, <b>Sikandar Lodi</b>, and <b>Major R. Smith</b>.<br><br><b>Alai Darwaza</b> was built during the reign of <b>Alauddin Khilji</b>. Other notable Sultanate-era structures include <b>Balban's Lal Mahal</b> and <b>Ukha Masjid</b> (built by <b>Mubarak Shah Khilji</b> in <b>Bayana, Rajasthan</b>).",
+    "expimage": ""
+  },
+  "14": {
+    "questext": "Which of the following buildings belong to the Tughlaq period?",
+    "options": [
+      "Quwwat-ul-Islam Mosque and Adhai Din Ka Jhonpra",
+      "Alai Darwaza and Ukha Masjid",
+      "Tughlaqabad Fort, Jahanpanah Nagar and Kotla Firoz Shah",
+      "Sikander Lodi's Tomb and Moth ki Masjid"
+    ],
+    "correct": "3",
+    "quesimage": "",
+    "exptext": "<b>Tughlaqabad Fort, Jahanpanah Nagar and Kotla Firoz Shah</b><br><br>The major buildings of the <b>Tughlaq period</b> include:<br>• <b>Tughlaqabad Fort</b><br>• <b>Jahanpanah Nagar</b><br>• <b>Ghiyasuddin's Tomb</b><br>• <b>Kotla Firoz Shah</b><br><br>Among the buildings of the <b>Lodi period</b>, <b>Sikander Lodi's Tomb</b> and <b>Moth ki Masjid</b> are quite famous.",
+    "expimage": ""
+  }
+};
