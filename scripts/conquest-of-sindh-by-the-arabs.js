5f53c10cbeb2c46ddeb4b0dc715a8ef23b2a98dc
Sudhanshu09102000 <127600086+Sudhanshu09102000@users.noreply.github.com>
2026-06-15 17:01:32 +0530

    Create conquest-of-sindh-by-the-arabs.js

---
+++ scripts/conquest-of-sindh-by-the-arabs.js
@@ -0,0 +1,158 @@
+window.TEST_DATA = {
+  "1": {
+    "questext": "Who led the first successful Muslim attack on India in 712 AD?",
+    "options": [
+      "Ubaidullah",
+      "Budail",
+      "Muhammad-bin-Qasim",
+      "Mahmud Ghaznavi"
+    ],
+    "correct": "3",
+    "quesimage": "",
+    "exptext": "<b>Muhammad-bin-Qasim</b><br><br>The first Muslim attack on India took place in <b>711 AD</b> under the leadership of <b>Ubaidullah</b>. The second attack also took place in <b>711 AD</b> under the leadership of <b>Budail</b>. Both these attacks <b>failed</b>.<br><br>Finally, the <b>first successful Muslim attack</b> on India took place in <b>712 AD</b> under the leadership of <b>Muhammad-bin-Qasim</b>. At that time, Sindh was ruled by <b>Dahir</b>.<br><br>The primary motive of the Arabs in attacking Sindh was to <b>loot wealth</b> and for the <b>propagation of Islam</b>. Before attacking India, they had subjugated vast areas of <b>Africa</b> and <b>Europe</b>.",
+    "expimage": ""
+  },
+  "2": {
+    "questext": "After the Arab invasion of 712 AD, which new powers emerged and ruled India for the next 300 years?",
+    "options": [
+      "Pala, Chola and Chalukya",
+      "Gurjar Pratihara, Rashtrakuta and Chalukya",
+      "Pallava, Rashtrakuta and Pala",
+      "Gurjar Pratihara, Chola and Pallava"
+    ],
+    "correct": "2",
+    "quesimage": "",
+    "exptext": "<b>Gurjar Pratihara, Rashtrakuta and Chalukya</b><br><br>In <b>712 AD</b>, after the defeat by the Arabs, many new powers emerged to face the upcoming challenges — namely the <b>Gurjar Pratihara</b>, <b>Rashtrakuta</b>, and <b>Chalukya</b> dynasties — who ruled India for the next <b>300 years</b>.<br><br>After the Arab invasion, the Indian peninsula remained safe from foreign invasions for a long period. However, around <b>1000 AD</b>, conditions of decentralization and division became active once again. As a result, the <b>Turks</b> attacked India a total of <b>17 times</b> under the leadership of <b>Mahmud Ghaznavi</b>.<br><br>The credit for <b>establishing Muslim rule in India</b> is given to the <b>Turks</b> rather than the Arabs.",
+    "expimage": ""
+  },
+  "3": {
+    "questext": "What was the political condition of India at the beginning of the eighth century?",
+    "options": [
+      "India was ruled by a single powerful central empire",
+      "India was divided into two large kingdoms",
+      "India was a collection of various small states with no supreme central power",
+      "India was under the control of the Arab Caliphate"
+    ],
+    "correct": "3",
+    "quesimage": "",
+    "exptext": "<b>India was a collection of various small states with no supreme central power</b><br><br>At the beginning of the <b>eighth century</b>, there was <b>no supreme central power</b> in India. The country was divided into a collection of various small independent states, which made it vulnerable to foreign invasions.<br><br>At the time of the Arab invasion, <b>Afghanistan</b> was ruled by a <b>Brahmin dynasty</b>. Muslim writers have called this dynasty the <b>Hindushahi Empire</b> or the Empire of <b>'Kabul'</b> or <b>'Jabul'</b>.",
+    "expimage": ""
+  },
+  "4": {
+    "questext": "Who founded the Karkota dynasty in Kashmir in the seventh century?",
+    "options": [
+      "Lalitaditya Muktapida",
+      "Yashovarman",
+      "Durlabhvardhana",
+      "Dahir"
+    ],
+    "correct": "3",
+    "quesimage": "",
+    "exptext": "<b>Durlabhvardhana</b><br><br>In the <b>seventh century</b>, <b>Durlabhvardhana</b> founded the <b>Karkota dynasty</b> in Kashmir.<br><br>Among the notable rulers of this dynasty was <b>Lalitaditya Muktapida</b>, who ascended the throne around <b>724 AD</b>. He was a majestic and powerful ruler of his dynasty. During his reign, the <b>Martand Temple</b> was built for the <b>Sun God</b>. Around <b>740 AD</b>, he defeated <b>King Yashovarman of Kannauj</b>.",
+    "expimage": ""
+  },
+  "5": {
+    "questext": "What happened to Arjuna, the ruler of Kannauj, after he opposed the Chinese ambassador Wang Hugese?",
+    "options": [
+      "He was killed in battle by Wang Hugese",
+      "He was taken prisoner to China and later died in prison",
+      "He fled to Tibet and sought asylum",
+      "He surrendered Kannauj to the Arabs"
+    ],
+    "correct": "2",
+    "quesimage": "",
+    "exptext": "<b>Arjuna was taken prisoner to China and later died in prison there</b><br><br>After <b>Harsha's death</b>, <b>Arjuna</b> (a local ruler) took over <b>Kannauj</b>. He opposed the <b>Chinese ambassador Wang Hugese</b>, who had arrived there following Harsha's death. Hugese returned with military help from <b>Assam</b>, <b>Tibet</b>, and <b>Nepal</b>. Arjuna was defeated in the war, taken prisoner to <b>China</b>, and later <b>died in prison</b> there.<br><br>In the beginning of the <b>eighth century</b>, <b>Yashovarman</b> ascended the throne of Kannauj. He was a contemporary of <b>King Dahir of Sindh</b>.",
+    "expimage": ""
+  },
+  "6": {
+    "questext": "The 'Tripartite Struggle' of the 8th century was fought among which three powers for supremacy over Kannauj?",
+    "options": [
+      "Chola, Pallava and Rashtrakuta",
+      "Pala, Gurjar Pratihara and Rashtrakuta",
+      "Chalukya, Pala and Gurjar Pratihara",
+      "Gurjar Pratihara, Chalukya and Chola"
+    ],
+    "correct": "2",
+    "quesimage": "",
+    "exptext": "<b>Pala, Gurjar Pratihara and Rashtrakuta</b><br><br>A war took place among three major powers of the <b>8th century</b> — the <b>Pala</b>, <b>Gurjar Pratihara</b>, and <b>Rashtrakuta</b> — for supremacy over <b>Kannauj</b>. This conflict is historically known as the <b>'Tripartite Struggle'</b>.<br><br>Ultimately, the <b>Pratiharas</b> won this war and emerged as an important power in <b>North India</b>.",
+    "expimage": ""
+  },
+  "7": {
+    "questext": "Who was the last ruler of the Rai dynasty of Sindh, and who succeeded him?",
+    "options": [
+      "Rai Sahasi I; succeeded by Dahir",
+      "Rai Sahasi II; succeeded by Brahmin minister Chach",
+      "Chach; succeeded by Chandar",
+      "Chandar; succeeded by Dahir"
+    ],
+    "correct": "2",
+    "quesimage": "",
+    "exptext": "<b>Rai Sahasi II; succeeded by Brahmin minister Chach</b><br><br><b>Sindh</b> was originally ruled by the <b>Rai dynasty</b>. When <b>Hiuen Tsang</b> visited India, he found a <b>Shudra ruler</b> in Sindh. <b>Harsha</b> had conquered Sindh during his reign, but Sindh became independent after Harsha's death.<br><br>The last ruler of the Rai dynasty was <b>Rai Sahasi II</b>. After him, the <b>Brahmin minister Chach</b> took over the kingdom and laid the foundation of the <b>Brahmin dynasty</b>. After Chach, <b>Chandar</b> ascended the throne, and after Chandar, <b>Dahir</b> became the ruler. It was this <b>King Dahir</b> who faced the Arabs during the invasion of Sindh.",
+    "expimage": ""
+  },
+  "8": {
+    "questext": "During Muhammad bin Qasim's Dewal campaign, who helped him win the port of Dewal despite strong opposition?",
+    "options": [
+      "Buddhist monks who accepted his suzerainty",
+      "Dahir's nephew who surrendered willingly",
+      "Deceitful Brahmins who supported Qasim",
+      "Arab traders already settled in Dewal"
+    ],
+    "correct": "3",
+    "quesimage": "",
+    "exptext": "<b>Deceitful Brahmins who supported Muhammad bin Qasim</b><br><br><b>Muhammad bin Qasim</b> first attacked <b>Dewal</b> via <b>Makran</b>. In <b>712 AD</b>, he reached the port of <b>Dewal</b> and surrounded it. <b>Dahir's nephew</b> strongly opposed him at Dewal, but due to the support of <b>deceitful Brahmins</b>, Muhammad bin Qasim soon won Dewal.<br><br>From Dewal, Qasim moved towards <b>Nirun</b>, where the <b>Buddhist monks</b> accepted his suzerainty <b>without a fight</b>. He then advanced to <b>Sehban</b>, which was ruled by <b>Dahir's cousin Majhra</b>. Here too, after some resistance, Qasim's suzerainty was accepted.",
+    "expimage": ""
+  },
+  "9": {
+    "questext": "On which date did the fierce battle between Raja Dahir and Muhammad bin Qasim take place at Rawar?",
+    "options": [
+      "10 June 711 AD",
+      "20 June 712 AD",
+      "15 July 712 AD",
+      "20 June 713 AD"
+    ],
+    "correct": "2",
+    "quesimage": "",
+    "exptext": "<b>20 June 712 AD</b><br><br>After the victory at Sehban, <b>Muhammad bin Qasim</b> crossed the <b>Sindh river</b> and attacked <b>Raja Dahir</b>. A fierce battle took place between the armies of <b>Dahir</b> and <b>Qasim</b> on <b>20 June 712 AD</b>. Ultimately, <b>Dahir died</b> during the battle.<br><br>After the death of Raja Dahir, his widow <b>Ranbai</b> valiantly defended the <b>fort of Rawar</b>. When she could no longer defend herself, she protected her honour by the practice of <b>Johar</b>.",
+    "expimage": ""
+  },
+  "10": {
+    "questext": "What happened after the fall of Brahmanabad during Muhammad bin Qasim's campaigns?",
+    "options": [
+      "Jai Singh surrendered and converted to Islam",
+      "Dahir's son Jai Singh fled after defeat and Dahir's widow and daughters were sent to the Caliph",
+      "Muhammad bin Qasim was recalled to Arabia after the battle",
+      "Dahir's widow Ranbai performed Johar at Brahmanabad"
+    ],
+    "correct": "2",
+    "quesimage": "",
+    "exptext": "<b>Jai Singh fled after defeat and Dahir's widow and daughters were sent to the Caliph in Damascus</b><br><br><b>Dahir's son Jai Singh</b> was ruling <b>Brahmanabad</b>. A fierce battle took place between the forces of <b>Muhammad bin Qasim</b> and <b>Jai Singh</b>, in which about <b>20,000 people died</b>. Ultimately, <b>Jai Singh fled</b> from the battlefield after being defeated.<br><br>After the fall of Brahmanabad, Muhammad bin Qasim imprisoned <b>Dahir's second widow Queen Lad Devi</b> and her two daughters — <b>Surya Devi</b> and <b>Parmal Devi</b> — and sent them to the <b>Caliph in Damascus</b>.<br><br>The task of protecting <b>Aror/Alor</b>, the capital of Sindh, was in the hands of another son of Dahir. Muhammad bin Qasim attacked and conquered it as well.",
+    "expimage": ""
+  },
+  "11": {
+    "questext": "Why did Multan come to be called the 'Golden City' after the Arab conquest?",
+    "options": [
+      "Because of its famous golden temple dedicated to the Sun God",
+      "Because the Arabs obtained a large amount of gold from its conquest",
+      "Because Multan was the wealthiest trading centre of Upper Sindh",
+      "Because Muhammad bin Qasim built a golden mosque there"
+    ],
+    "correct": "2",
+    "quesimage": "",
+    "exptext": "<b>The Arabs obtained a large amount of gold from the conquest of Multan</b><br><br>In the beginning of <b>713 AD</b>, <b>Muhammad bin Qasim</b> advanced towards <b>Multan</b>, the main city of <b>Upper Sindh</b>. It is believed that the Arabs obtained a large amount of <b>gold</b> from the conquest of Multan, due to which Multan came to be called the <b>'Golden City'</b>.<br><br>After the conquest of <b>Sindh and Multan</b>, Muhammad bin Qasim started making plans to conquer the rest of India. However, <b>Muhammad bin Qasim died</b> before he could conquer any area beyond Sindh and Multan.",
+    "expimage": ""
+  },
+  "12": {
+    "questext": "Which historian remarked that the Arab conquest of Sindh was 'merely a historical and Islamic event' and an 'ineffective conquest'?",
+    "options": [
+      "Al-Biruni",
+      "Lanepool",
+      "Ibn Battuta",
+      "Ziauddin Barani"
+    ],
+    "correct": "2",
+    "quesimage": "",
+    "exptext": "<b>Lanepool</b><br><br>Regarding the impact of the Sindh conquest, <b>Lanepool</b> stated that <i>"Sindh was conquered by the Arabs, but this conquest was merely a historical and Islamic event. It was an <b>ineffective conquest</b>."</i><br><br>Despite this, the Arab conquest had significant cultural consequences. <b>Vishnu Sharma's</b> <b>'Panchatantra'</b> was translated into Arabic under the name <b>'Kalilawadadimna'</b>.<br><br>In the <b>eighth century</b>, Arab scholars took with them <b>'Brahmasiddhanta'</b> written by <b>Brahmagupta</b> and <b>'Khandankhandkhady'</b> written by <b>Shri Harsha</b> to Arabia, where they were translated into Arabic with the help of Indian scholars.<br><br><b>Muhammad bin Qasim</b> was the <b>first Muslim invader in India</b> who collected <b>Jaziya tax</b>.",
+    "expimage": ""
+  }
+};
