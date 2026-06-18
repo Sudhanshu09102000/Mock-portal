ec3146accd2242eb71adb8a1f9dab290ad8d2d63
Sudhanshu09102000 <127600086+Sudhanshu09102000@users.noreply.github.com>
2026-06-19 01:45:03 +0530

    Create lodi-dynasty.js

---
+++ scripts/lodi-dynasty.js
@@ -0,0 +1,82 @@
+window.TEST_DATA = {
+  "1": {
+    "questext": "Which dynasty ruled the Delhi Sultanate from 1451 to 1526 CE?",
+    "options": ["Sayyid Dynasty", "Lodi Dynasty", "Tughlaq Dynasty", "Khalji Dynasty"],
+    "correct": "2",
+    "quesimage": "",
+    "exptext": "<b>Lodi Dynasty (1451–1526 CE)</b><br><br>The <b>Lodi Dynasty</b> ruled the Delhi Sultanate for <b>75 years</b> (1451–1526 CE). They were <b>Afghans</b> divided into a number of clans. The Afghans were enrolled in large numbers in the army of the Delhi Sultanate in the <b>second half of the thirteenth century</b>.<br><br>The foundation of the <b>first Afghan dynasty</b> in the Delhi Sultanate was laid by <b>Bahlol Lodi</b>. There were a total of <b>three rulers</b> of the Lodi Dynasty:<br>• <b>Bahlol Lodi</b> (1451–1489 CE)<br>• <b>Sikandar Lodi</b> (1489–1517 CE)<br>• <b>Ibrahim Lodi</b> (1517–1526 CE)",
+    "expimage": ""
+  },
+  "2": {
+    "questext": "Who was the founder of the Lodi Dynasty?",
+    "options": ["Sikandar Lodi", "Ibrahim Lodi", "Bahlol Lodi", "Jalal Khan"],
+    "correct": "3",
+    "quesimage": "",
+    "exptext": "<b>Bahlol Lodi (1451–1489 CE)</b><br><br><b>Bahlol Lodi</b> was the founder of the <b>Lodi Dynasty</b>. Before becoming the Sultan, he served as the <b>Governor of Sirhind</b>.<br><br>His greatest success was defeating the <b>Sharqi ruler of Jaunpur, Hussain Shah</b>, and re-annexing the <b>Jaunpur state</b> into his empire. He appointed his son <b>Barbak Lodi</b> as the ruler of Jaunpur.<br><br>Bahlol Lodi issued a silver coin called <b>Bahloli</b>. In <b>1486–87 AD</b>, he defeated <b>Rai Karna (Kirtidev)</b>, the ruler of Gwalior — this <b>Gwalior campaign</b> was his last campaign. While returning from Gwalior, he fell ill and died on the way to Delhi in <b>July 1489</b>.",
+    "expimage": ""
+  },
+  "3": {
+    "questext": "What was the silver coin issued by Bahlol Lodi called?",
+    "options": ["Bahloli", "Tanka", "Jital", "Sikandari"],
+    "correct": "1",
+    "quesimage": "",
+    "exptext": "<b>Bahloli</b><br><br>Bahlol Lodi issued a silver coin called <b>Bahloli</b>. This coin is historically significant as it was one of the notable economic contributions of the <b>Lodi Dynasty</b> to the Delhi Sultanate.<br><br>Bahlol Lodi's last military campaign was the <b>Gwalior campaign (1486–87 AD)</b>, in which he defeated <b>Rai Karna (Kirtidev)</b>, the ruler of Gwalior. He fell ill while returning and died on the way to Delhi in <b>July 1489</b>.",
+    "expimage": ""
+  },
+  "4": {
+    "questext": "What was the original name of Sikandar Lodi before he ascended the throne of Delhi?",
+    "options": ["Barbak Khan", "Jalal Khan", "Nizam Khan", "Alam Khan"],
+    "correct": "3",
+    "quesimage": "",
+    "exptext": "<b>Nizam Khan (Sikandar Lodi)</b><br><br>The second surviving son of Bahlol Lodi, <b>Nizam Khan</b>, ascended the throne of Delhi under the title of <b>Sikandar Shah</b>. There was initial hesitation among the Amirs and chieftains in accepting him as Sultan because his <b>mother was a Hindu</b>. However, Sikandar adopted a policy of <b>intolerance towards Hindus</b> and established himself as a <b>religious fanatic Afghan Muslim</b>, similar in character to <b>Firuz Shah Tughlaq</b>.<br><br>He founded the city of <b>Agra in 1505 AD</b> (some sources cite 1504–1506). He destroyed numerous <b>Hindu temples</b> during his reign.",
+    "expimage": ""
+  },
+  "5": {
+    "questext": "Which city was founded by Sikandar Lodi in 1505 AD?",
+    "options": ["Lucknow", "Agra", "Lahore", "Fatehpur"],
+    "correct": "2",
+    "quesimage": "",
+    "exptext": "<b>Agra</b><br><br>Sikandar Lodi founded the city of <b>Agra in 1505 AD</b> (some sources cite 1504–1506). He destroyed numerous <b>Hindu temples</b> during his reign, drawing comparison to <b>Firuz Shah Tughlaq</b>.<br><br>In <b>1492 AD</b>, Sikandar Lodi dethroned <b>Barbak Shah</b> and annexed <b>Jaunpur</b> to the Delhi Sultanate after Barbak Shah failed to suppress the revolt of supporters of the Sharqi ruler <b>Hussain Shah</b>. In <b>1494 AD</b>, Sikandar defeated <b>Hussain Shah</b> in a battle near <b>Banaras</b>.",
+    "expimage": ""
+  },
+  "6": {
+    "questext": "What was the land measurement scale developed by Sikandar Lodi called?",
+    "options": ["Gaz-e-Ilahi", "Gaz-e-Sikandari", "Gaz-e-Shahi", "Gaz-e-Sultani"],
+    "correct": "2",
+    "quesimage": "",
+    "exptext": "<b>Gaz-e-Sikandari</b><br><br>Sikandar Lodi developed a scale for measuring land known as <b>Gaz-e-Sikandari</b>. This was one of his notable administrative contributions.<br><br>He also imposed several social restrictions:<br>• He stopped the procession of <b>Tazia</b> during <b>Muharram</b>.<br>• He prohibited <b>women from visiting the tombs of saints</b> or taking out processions in their memory.<br>• He prohibited <b>Hindus from bathing in the Yamuna</b> and also stopped them from cutting their hair there.",
+    "expimage": ""
+  },
+  "7": {
+    "questext": "Under which pen name did Sikandar Lodi write Persian poetry?",
+    "options": ["Gulbadan", "Gulrukhi", "Gulistan", "Gulfam"],
+    "correct": "2",
+    "quesimage": "",
+    "exptext": "<b>Gulrukhi</b><br><br>Sikandar Lodi was well versed in the <b>Persian language</b> and wrote poems in Persian under the pen name <b>'Gulrukhi'</b>.<br><br>Several important literary and scholarly works were produced during his reign:<br>• <b>Lajjat-e-Sikandershahi</b> — a book on <b>music</b> written during his time.<br>• <b>Farhang-e-Sikandari</b> — a translated book on <b>Ayurveda</b>.<br>• <b>Tibb-e-Sikandari</b> — a book on <b>medicine</b> written by his minister <b>Miyan Bhuan</b>, a renowned scholar.<br><br>Sikandar Lodi died in <b>1517 AD</b> due to a <b>throat disease</b>.",
+    "expimage": ""
+  },
+  "8": {
+    "questext": "Which book on medicine was written by Sikandar Lodi's minister Miyan Bhuan?",
+    "options": ["Lajjat-e-Sikandershahi", "Farhang-e-Sikandari", "Tibb-e-Sikandari", "Kitab-ul-Hind"],
+    "correct": "3",
+    "quesimage": "",
+    "exptext": "<b>Tibb-e-Sikandari</b><br><br>Sikandar Lodi's minister <b>Miyan Bhuan</b> was a renowned scholar who wrote a book on medicine called <b>Tibb-e-Sikandari</b>.<br><br>Other important works from the reign of Sikandar Lodi include:<br>• <b>Lajjat-e-Sikandershahi</b> — a book on <b>music</b>.<br>• <b>Farhang-e-Sikandari</b> — a translated book on <b>Ayurveda</b>.<br><br>Sikandar Lodi himself wrote Persian poetry under the pen name <b>'Gulrukhi'</b>, reflecting his deep interest in literary and cultural pursuits despite his religiously intolerant political stance.",
+    "expimage": ""
+  },
+  "9": {
+    "questext": "In which battle did Ibrahim Lodi face defeat against Rana Sanga of Mewar in 1517–18?",
+    "options": ["Battle of Panipat", "Battle of Khanwa", "Battle of Khatoli", "Battle of Chanderi"],
+    "correct": "3",
+    "quesimage": "",
+    "exptext": "<b>Battle of Khatoli (1517–18)</b><br><br>Ibrahim Lodi, the eldest son of Sikandar Lodi and the <b>last ruler of the Lodi Dynasty</b>, was encouraged by his victory at <b>Gwalior in 1518 AD</b> and subsequently invaded <b>Mewar</b>. He faced <b>Rana Sanga</b>, the ruler of Mewar, in the <b>Battle of Khatoli (1517–18)</b>, in which <b>Ibrahim Lodi was defeated</b>.<br><br>Ibrahim's reign was also marked by internal conflict — his younger brother <b>Jalal Khan</b> was placed on the throne at <b>Jaunpur</b> by Afghan chieftains seeking to divide the empire. In the subsequent struggle for succession, <b>Jalal Khan was poisoned to death in prison</b>.",
+    "expimage": ""
+  },
+  "10": {
+    "questext": "Who invited Babur to invade India in order to dislodge Ibrahim Lodi?",
+    "options": ["Rana Sanga and Jalal Khan", "Daulat Khan Lodi and Alam Khan", "Barbak Shah and Hussain Shah", "Miyan Bhuan and Alam Khan"],
+    "correct": "2",
+    "quesimage": "",
+    "exptext": "<b>Daulat Khan Lodi and Alam Khan</b><br><br><b>Daulat Khan Lodi</b>, the <b>Governor of Punjab</b>, and <b>Alam Khan</b>, the uncle of the Sultan, sent envoys to <b>Babur</b>, the ruler of Kabul, with an invitation to invade India and dislodge <b>Ibrahim Lodi</b>.<br><br>This invitation came as a result of Ibrahim Lodi's <b>arrogant and distrustful behaviour</b> towards the chieftains of his father's time — he either deposed, imprisoned, or had them killed. Consequently, during Babur's invasion, the chieftains deserted Ibrahim Lodi on the battlefield.<br><br>Babur invaded India and defeated and killed <b>Ibrahim Lodi</b> in the <b>Battle of Panipat (1526)</b>. This marked:<br>• The <b>end of the Lodi Dynasty</b>.<br>• The <b>downfall of the Delhi Sultanate</b>.<br>• The <b>beginning of the Mughal Empire</b>.<br><br><b>Ibrahim Lodi</b> holds the distinction of being the <b>only Sultan of medieval India who was killed on the battlefield</b>.",
+    "expimage": ""
+  }
+};
