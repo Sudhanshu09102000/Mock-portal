0c7462d7c301bde4735264861dd3e1239ea8abed
Sudhanshu09102000 <127600086+Sudhanshu09102000@users.noreply.github.com>
2026-06-20 00:06:19 +0530

    Create mughal-period-babur.js

---
+++ scripts/mughal-period-babur.js
@@ -0,0 +1,288 @@
+window.TEST_DATA = {
+  "1": {
+    "questext": "Timur's expansionist campaign in Central Asia, developments from which eventually led to Babur's arrival in India, continued for almost a quarter of a century until his death in which year?",
+    "options": [
+      "1398-99",
+      "1404-05",
+      "1410-11",
+      "1420-21"
+    ],
+    "correct": "2",
+    "quesimage": "",
+    "exptext": "<b>1404-05 AD</b><br><br>During the <b>14th century</b>, <b>Timur</b> embarked upon an expansionist approach in <b>Central Asia</b>, which lasted almost a <b>quarter of a century</b> until his death in <b>1404-05</b>. The developments in Central Asia during the <b>15th and early 16th centuries</b> led to a series of events that culminated in the arrival of <b>Zahiruddin Muhammad Babur</b> on Indian soil.",
+    "expimage": ""
+  },
+  "2": {
+    "questext": "Babur, a descendant of Timur and founder of the Mughal Empire, was expelled by the Uzbek rulers from his ancestral state. What was the name of this state?",
+    "options": [
+      "Kabul",
+      "Fargana",
+      "Kandahar",
+      "Samarkand"
+    ],
+    "correct": "2",
+    "quesimage": "",
+    "exptext": "<b>Fargana</b><br><br>Babur was one of the descendants of <b>Timur</b> and became the founder of the <b>Mughal Empire</b>. His ancestral state was <b>Fargana</b>. The <b>Uzbek rulers</b> of Central Asia expelled Babur from Fargana after a long struggle.<br><br>Babur then conquered the state of <b>Kabul in 1504 CE</b>, and after some time he also succeeded in conquering <b>Kandahar</b> and <b>Samarkand</b>, after which Babur turned his attention towards India.",
+    "expimage": ""
+  },
+  "3": {
+    "questext": "Zahir-ud-Din Muhammad Babur, founder of Mughal rule in India, was born in 1483 in which region?",
+    "options": [
+      "Kabul",
+      "Samarkand",
+      "Fergana",
+      "Ghazni"
+    ],
+    "correct": "3",
+    "quesimage": "",
+    "exptext": "<b>Fergana, Uzbekistan</b><br><br>Babur, the founder of the Mughal rule in India, was born in <b>1483</b> in <b>Fergana, Uzbekistan</b>. He was a direct descendant of <b>Timur</b>, who had invaded India in <b>1398</b>. On his mother's side, he was related to the Mongol conqueror, <b>Genghis Khan</b>.<br><br>Babur had a great fascination for <b>Samarqand</b>, the capital of Timur.",
+    "expimage": ""
+  },
+  "4": {
+    "questext": "The new dynasty founded by Babur, of Turkish origin and named after the second son of Genghis Khan, is generally known by which name?",
+    "options": [
+      "Timurid dynasty",
+      "Mughal dynasty",
+      "Lodi dynasty",
+      "Sayyid dynasty"
+    ],
+    "correct": "2",
+    "quesimage": "",
+    "exptext": "<b>Mughal dynasty</b><br><br>Zahiruddin Muhammad was called <b>'Babur'</b> because of his valour and fearlessness. Babur's father was a descendant of <b>Timur</b> and his mother was a descendant of the <b>Mongols</b>; thus, he had a mixture of the blood of both Turks and Mongols.<br><br>The new dynasty founded by Babur was the <b>'Chagatai' dynasty</b> of Turkish origin. It was named after the second son of <b>Genghis Khan</b>, but is generally called the <b>'Mughal dynasty'</b>.",
+    "expimage": ""
+  },
+  "5": {
+    "questext": "In which year did Babur assume the title of 'Padshah' (King), a title he held in place of his earlier title of 'Mirza'?",
+    "options": [
+      "1501 CE",
+      "1504 CE",
+      "1507 CE",
+      "1494 CE"
+    ],
+    "correct": "3",
+    "quesimage": "",
+    "exptext": "<b>1507 CE</b><br><br>Babur ascended the throne of <b>Fargana</b> in <b>1494 CE</b> at the young age of <b>11 years</b> after the death of his father. In <b>1501 CE</b>, he captured <b>Samarkand</b>, which remained in his possession for only eight months.<br><br>After conquering <b>Kabul in 1504 CE</b>, Babur gained control over Kabul and <b>Ghazni</b>. In <b>1507 CE</b>, Babur assumed the title of <b>'Padshah' (King)</b>. Before assuming this title, Babur used to assume the title of <b>'Mirza'</b>.",
+    "expimage": ""
+  },
+  "6": {
+    "questext": "What was one of the main reasons for Babur's desire to conquer India?",
+    "options": [
+      "Religious conversion of Indian rulers",
+      "Kabul gave very little income, insufficient for his army's needs",
+      "A request from Mughal nobles in Delhi",
+      "Desire to unify all of Central Asia"
+    ],
+    "correct": "2",
+    "quesimage": "",
+    "exptext": "<b>Kabul gave very little income, which did not meet the needs of the army</b><br><br>Babur's attack on India was the result of repeated defeats from the powerful <b>Uzbeks</b> in Central Asia. One of the reasons for his desire to conquer India was that <b>Kabul</b> gave very little income, which did not meet the needs of the army. Due to this greed for money, he turned towards India.",
+    "expimage": ""
+  },
+  "7": {
+    "questext": "Babur's first campaign against India (1518-1519 CE), in which he captured Bajaur and Bhera/Bhira, was directed against which tribe?",
+    "options": [
+      "Afridi tribe",
+      "Yusafzai tribe",
+      "Mohmand tribe",
+      "Khattak tribe"
+    ],
+    "correct": "2",
+    "quesimage": "",
+    "exptext": "<b>Yusafzai tribe</b><br><br>Babur's first campaign against India was against the <b>'Yusafzai' tribe</b> in <b>1518-1519 CE</b>. In this campaign, Babur captured <b>'Bajaur'</b> and <b>'Bhera/Bhira'</b>.<br><br>Babur wrote in his autobiography (<b>Baburnama</b>) that he used <b>gunpowder and cannons</b> to conquer this fort (Bajaur).",
+    "expimage": ""
+  },
+  "8": {
+    "questext": "After crossing the Khyber Pass in 1519 CE, Babur again conquered Bajaur and Bhera/Bhira in 1520 CE, along with which other two towns?",
+    "options": [
+      "Lahore and Multan",
+      "Sialkot and Syedpur",
+      "Delhi and Agra",
+      "Kabul and Ghazni"
+    ],
+    "correct": "2",
+    "quesimage": "",
+    "exptext": "<b>Sialkot and Syedpur</b><br><br>In <b>1519 CE</b>, Babur crossed the <b>Khyber Pass (Peshawar)</b>. In <b>1520 CE</b>, he again conquered <b>'Bajaur and Bhera/Bhira'</b>, and also captured <b>'Sialkot'</b> and <b>'Syedpur'</b>.",
+    "expimage": ""
+  },
+  "9": {
+    "questext": "During Babur's Peshawar campaign in 1524 CE, which Punjab governor sent an invitation (through his son Dilawar Khan) urging Babur to attack India, exploiting his differences with Ibrahim Lodi?",
+    "options": [
+      "Daulat Khan",
+      "Rana Sanga",
+      "Mahmud Lodhi",
+      "Alam Khan"
+    ],
+    "correct": "1",
+    "quesimage": "",
+    "exptext": "<b>Daulat Khan</b><br><br>During Babur's Peshawar campaign in <b>1524 CE</b>, Babur got the news of differences between <b>Ibrahim Lodi</b> and <b>Daulat Khan</b>, due to which Daulat Khan (who was the governor of Punjab at that time) sent a message to Babur through his son <b>Dilawar Khan</b> to attack India. Probably at the same time, <b>Rana Sanga</b> also sent an invitation to Babur to attack India.",
+    "expimage": ""
+  },
+  "10": {
+    "questext": "The First Battle of Panipat, fought between Babur and Ibrahim Lodi, took place on which date?",
+    "options": [
+      "21 April 1526",
+      "26 December 1530",
+      "17 March 1527",
+      "30 January 1528"
+    ],
+    "correct": "1",
+    "quesimage": "",
+    "exptext": "<b>21 April 1526</b><br><br>The First Battle of Panipat was fought between the armies of <b>Ibrahim Lodi</b> and the Mughal Emperor, <b>Babur</b>. Ibrahim Lodi was defeated in the battle, which took place on <b>21st April, 1526</b>. By his clever leadership, Babur defeated a very large Afghan regiment with only <b>twelve thousand soldiers</b>. Ibrahim Lodi died on the same day, <b>21 April 1526</b>.",
+    "expimage": ""
+  },
+  "11": {
+    "questext": "After the victory at Panipat, the Sultanate at Delhi ended and Babur's troops occupied Delhi and Agra. Babur gifted a silver coin to every resident of Kabul to celebrate, earning him which title for his generosity?",
+    "options": [
+      "Ghazi",
+      "Kalandar",
+      "Padshah",
+      "Mirza"
+    ],
+    "correct": "2",
+    "quesimage": "",
+    "exptext": "<b>Kalandar</b><br><br>With Ibrahim Lodi's defeat, the <b>Sultan Shahi (Delhi Sultanate)</b> ended in India, and Babur's troops occupied both <b>Delhi</b> and <b>Agra</b>. In the battle of Panipat, Babur's artillery was led by two capable officers named <b>Ustad Ali</b> and <b>Mustafa Khan</b>.<br><br>To celebrate the victory, Babur gifted a silver coin to each Kabul resident. Due to his generosity, he was given the title of <b>'Kalandar'</b>. Babur gave the credit of victory in this war to his <b>archers</b>.",
+    "expimage": ""
+  },
+  "12": {
+    "questext": "What was the main reason behind the Battle of Khanwa (March 1527 CE) fought between Babur and Rana Sanga?",
+    "options": [
+      "Babur's decision to stay in India after the Panipat war instead of returning",
+      "A territorial dispute over the fort of Chittor",
+      "Rana Sanga's refusal to pay tribute to Babur",
+      "Babur's attack on Mewar's temples"
+    ],
+    "correct": "1",
+    "quesimage": "",
+    "exptext": "<b>Babur's decision to stay in India after the Panipat war</b><br><br>The main reason for the Khanwa war was Babur's decision to stay in India after the Panipat war. <b>Rana Sanga</b> believed that Babur, like other foreign invaders, would loot the country and go back; probably for this reason, he had assured Babur of help against <b>Ibrahim Lodi</b>.<br><br>Several important <b>Rajput rulers</b> and <b>Afghan chiefs</b>, including Ibrahim Lodi's younger brother, rallied behind Rana Sanga.",
+    "expimage": ""
+  },
+  "13": {
+    "questext": "After winning the Battle of Khanwa, Babur imposed a complete ban on alcohol, gave the slogan of 'Jihad', and assumed which title?",
+    "options": [
+      "Kalandar",
+      "Padshah",
+      "Ghazi",
+      "Mirza"
+    ],
+    "correct": "3",
+    "quesimage": "",
+    "exptext": "<b>Ghazi</b><br><br>The <b>Mughals</b> won the Battle of Khanwa. However, on <b>30th January, 1528</b>, <b>Rana Sanga</b> died in <b>Chittor</b>, apparently poisoned by his own chiefs.<br><br>After winning the Battle of Khanwa, Babur assumed the title of <b>'Ghazi'</b>. In this battle, Babur also imposed a complete ban on <b>alcohol</b> and gave the slogan of <b>'Jihad'</b>.",
+    "expimage": ""
+  },
+  "14": {
+    "questext": "Babur's Battle of Chanderi (1528 CE) was fought against which Rajput chief, who refused a Jagir offered in exchange for the fort of Chanderi?",
+    "options": [
+      "Rana Sanga",
+      "Medini Rai",
+      "Mahmud Lodhi",
+      "Hemu"
+    ],
+    "correct": "2",
+    "quesimage": "",
+    "exptext": "<b>Medini Rai</b><br><br>Babur marched against <b>Chanderi</b>, which was a stronghold of the Rajputs under <b>Medini Rai</b>, reaching there in <b>January 1528</b>. Medini Rai refused to enter into any treaty with Babur and did not accept Babur's offer of a <b>Jagir</b> in lieu of Chanderi.",
+    "expimage": ""
+  },
+  "15": {
+    "questext": "After the fall of the Chanderi fort in January 1528, where the Rajput women performed Jauhar, what was the broader consequence for Babur's authority in India?",
+    "options": [
+      "No other Rajput chief could challenge Babur's authority thereafter",
+      "Babur lost control of Agra",
+      "Rana Sanga regained his lost territories",
+      "The Mughal Empire was dissolved"
+    ],
+    "correct": "1",
+    "quesimage": "",
+    "exptext": "<b>No other Rajput chief could challenge the authority of Babur</b><br><br>The Rajputs at Chanderi were determined to fight to the finish, and their women burnt themselves by performing <b>Jauhar</b>. Almost all the Rajputs lost their lives.<br><br>In <b>January 1528</b>, the <b>Fort of Chanderi</b> was captured. After this, no other Rajput chief could challenge the authority of Babur.",
+    "expimage": ""
+  },
+  "16": {
+    "questext": "The Battle of Ghagra (1529 CE), fought near Patna against the Afghans of Bihar led by Mahmud Lodhi, is associated with the large-scale introduction of which military technology in India, generally ascribed to Babur?",
+    "options": [
+      "War elephants",
+      "Cannons and muskets",
+      "Mounted cavalry archery",
+      "Siege towers"
+    ],
+    "correct": "2",
+    "quesimage": "",
+    "exptext": "<b>Cannons and muskets</b><br><br>The Afghans of Bihar were led by <b>Mahmud Lodhi</b>, the younger brother of Sultan <b>Ibrahim Lodhi</b>. Babur met the Afghans in the <b>Battle of Ghagra</b> (near Patna) in <b>May 1529</b> and won an easy victory.<br><br>The introduction of <b>cannons and muskets</b> in India in warfare on a massive scale has generally been ascribed to Babur.",
+    "expimage": ""
+  },
+  "17": {
+    "questext": "Babur died in Agra on 26 December 1530. As per his last wish, where was his body eventually taken and buried?",
+    "options": [
+      "Delhi",
+      "Agra",
+      "Kabul",
+      "Samarkand"
+    ],
+    "correct": "3",
+    "quesimage": "",
+    "exptext": "<b>Kabul</b><br><br>Babur died in <b>Agra</b> on <b>26 December 1530</b>. After his death, his body was kept in <b>'Arambag'</b> of Agra, but later, as per Babur's last wish, his body was taken to <b>Kabul</b> and buried, where his tomb is built.",
+    "expimage": ""
+  },
+  "18": {
+    "questext": "Babur introduced a unit called 'Gaz-e-Baabri', which was used for what purpose?",
+    "options": [
+      "Measuring the weight of gold",
+      "Measuring roads",
+      "Calculating land revenue",
+      "Counting army strength"
+    ],
+    "correct": "2",
+    "quesimage": "",
+    "exptext": "<b>Measuring roads</b><br><br>Babur used artillery and the new <b>'Tulgama Ganga' war method</b> for the first time in war. He introduced <b>'Gaz-e-Baabri'</b> for measuring roads.<br><br>Babur also brought two important games with him to India — <b>Ganjifa</b> (a card game) and <b>Ishqbaazi</b> (a pigeon game). Ishqbaazi was his favourite game.",
+    "expimage": ""
+  },
+  "19": {
+    "questext": "Babur, a scholar of the Turkish language, wrote his autobiography 'Baburnama' (also known as 'Tuzuk-e-Baabri') in which language?",
+    "options": [
+      "Persian",
+      "Arabic",
+      "Turkish",
+      "Chagatai-Mongol"
+    ],
+    "correct": "3",
+    "quesimage": "",
+    "exptext": "<b>Turkish</b><br><br>Babur wrote a book named <b>'Risal-e-Usaj'</b> on rhetoric. He was a scholar of the <b>Turkish language</b> and wrote his autobiography, <b>'Baburnama'</b> or <b>'Tuzuk-e-Baabri'</b>, in the Turkish language.",
+    "expimage": ""
+  },
+  "20": {
+    "questext": "Babur's garden in Agra, laid out using the 'geometrical method' and originally named 'Noor-e-Afghan', is known today by which name?",
+    "options": [
+      "Shalimar Bagh",
+      "Aram Bagh",
+      "Nishat Bagh",
+      "Lodi Gardens"
+    ],
+    "correct": "2",
+    "quesimage": "",
+    "exptext": "<b>Aram Bagh</b><br><br>Babur was very fond of planting gardens. He planted a garden in Agra using the <b>'geometrical method'</b>, which was called <b>'Noor-e-Afghan'</b>, but it is now called <b>'Aram Bagh'</b>.<br><br>Babur also issued a silver coin named <b>'Shahrukh'</b>.",
+    "expimage": ""
+  },
+  "21": {
+    "questext": "In the Baburnama, Babur mentioned five Muslim states — Bengal, Delhi, Malwa, Gujarat and Bahamani — along with which two Hindu states?",
+    "options": [
+      "Vijayanagar and Mewar",
+      "Vijayanagar and Orissa",
+      "Mewar and Assam",
+      "Orissa and Assam"
+    ],
+    "correct": "1",
+    "quesimage": "",
+    "exptext": "<b>Vijayanagar and Mewar</b><br><br>In the <b>'Baburnama'</b>, Babur mentioned <b>5 Muslim states</b> — <b>Bengal, Delhi, Malwa, Gujarat and Bahamani</b> — and <b>two Hindu states</b>, <b>Vijayanagar</b> and <b>Mewar</b>.",
+    "expimage": ""
+  },
+  "22": {
+    "questext": "In the Baburnama, Babur described which ruler of Vijayanagar as the most powerful ruler of contemporary India?",
+    "options": [
+      "Krishnadev Rai",
+      "Achyuta Deva Raya",
+      "Sadashiva Raya",
+      "Rama Raya"
+    ],
+    "correct": "1",
+    "quesimage": "",
+    "exptext": "<b>Krishnadev Rai</b><br><br>In 'Baburnama', Babur described the ruler of Vijayanagar, <b>'Krishnadev Rai'</b>, as the most powerful ruler of contemporary India.<br><br>Babur has also written that <b>Hindustan</b> is a very big country and there is immense wealth of gold and silver here. He famously wrote, \"Indians know how to die, not how to fight.\"",
+    "expimage": ""
+  }
+    };
