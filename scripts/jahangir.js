4694bec545525b0715f8e39f21e85e9a9fc41d62
Sudhanshu09102000 <127600086+Sudhanshu09102000@users.noreply.github.com>
2026-06-20 18:47:04 +0530

    Create jahangir.js

---
+++ scripts/jahangir.js
@@ -0,0 +1,276 @@
+
+window.TEST_DATA = {
+  "1": {
+    "questext": "Mughal Emperor Jahangir, whose earlier name was Nuruddin Muhammad Salim, was born to which parents?",
+    "options": [
+      "Akbar and Jodha Bai",
+      "Akbar and Mariam Uzzamani",
+      "Humayun and Hamida Banu Begum",
+      "Shah Jahan and Mumtaz Mahal"
+    ],
+    "correct": "2",
+    "quesimage": "",
+    "exptext": "<b>Akbar and Mariam Uzzamani</b><br><br>Mughal Emperor <b>Jahangir</b> was the son of <b>Akbar</b>. His earlier name was <b>Nuruddin Muhammad Salim</b>. His mother was the princess of <b>Jaipur</b>.<br><br>Jahangir was born in <b>August 1569 CE</b> to Akbar's wife <b>Mariam Uzzamani</b> with the blessings of <b>Sheikh Salim Chishti</b>, hence he was named <b>Salim</b>.",
+    "expimage": ""
+  },
+  "2": {
+    "questext": "After Akbar's death in 1605 CE, Salim became the Mughal Emperor under which title?",
+    "options": [
+      "Nuruddin Muhammad Jahangir Badshah Ghazi",
+      "Shahanshah Akbar-e-Azam",
+      "Abul Fath Jalaluddin Muhammad",
+      "Zahiruddin Muhammad Babur"
+    ],
+    "correct": "1",
+    "quesimage": "",
+    "exptext": "<b>Nuruddin Muhammad Jahangir Badshah Ghazi</b><br><br>After <b>Akbar's death in 1605 CE</b>, Salim became the ruler of the Mughal Empire with the title of <b>'Nuruddin Muhammad Jahangir Badshah Ghazi'</b>.<br><br>Some feudal lords in Akbar's court were in favour of making his elder son <b>Prince Khusro</b> the emperor in place of Jahangir. <b>Raja Mansingh</b> and <b>Mirza Aziz Koka</b> were prominent among them.<br><br>Before his death, <b>Akbar</b> himself placed the royal crown on Salim's head and recognized him as the heir to the throne.",
+    "expimage": ""
+  },
+  "3": {
+    "questext": "Which Sikh Guru was executed by Jahangir for supporting the rebellion of Prince Khusro, leading to bitterness between Sikhs and Mughals?",
+    "options": [
+      "Guru Nanak Dev",
+      "Guru Arjun Dev",
+      "Guru Tegh Bahadur",
+      "Guru Hargobind"
+    ],
+    "correct": "2",
+    "quesimage": "",
+    "exptext": "<b>Guru Arjun Dev</b><br><br>During the coronation of Jahangir, <b>Khusro</b> was imprisoned in the <b>Agra Fort</b> to suppress his rebellion. Later, due to his cleverness, he managed to escape from the Agra Fort. Thereafter, Khusro rebelled again with some of his supporters. Sikh Guru <b>Arjun Dev</b> also supported him.<br><br>Emperor Jahangir killed the fifth Sikh Guru <b>Arjun Dev</b> for supporting Prince Khusro. Due to this, a lot of bitterness developed between the <b>Sikhs</b> and the <b>Mughals</b>.<br><br>Khusro was murdered during the southern campaign with <b>Khurram</b>. Later, his body was brought to <b>Allahabad</b> and buried.",
+    "expimage": ""
+  },
+  "4": {
+    "questext": "Noor Jahan, Jahangir's wife whom he married in 1611 CE, was originally known by which childhood name?",
+    "options": [
+      "Ladli Begum",
+      "Asmat Begum",
+      "Mehrunnisa",
+      "Arjumand Banu"
+    ],
+    "correct": "3",
+    "quesimage": "",
+    "exptext": "<b>Mehrunnisa</b><br><br><b>Noor Jahan</b> was Jahangir's wife. They were married in <b>1611 CE</b>. After marriage, she received the title of <b>'Noormahal'</b>. Later this title was changed to <b>'Noor Jahan'</b> (light of the world).<br><br>Noor Jahan's childhood name was <b>Mehrunnisa</b>. Her father <b>Giyas Beg</b> was a resident of <b>Persia</b>. Jahangir gave Giyas Beg the title of <b>'Itmaduddaula'</b>.",
+    "expimage": ""
+  },
+  "5": {
+    "questext": "The influential 'Noor Jahan Group' during Jahangir's reign included Itmaduddaula, Asmat Begum, Asaf Khan and which prince?",
+    "options": [
+      "Prince Khusro",
+      "Prince Khurram",
+      "Prince Parvez",
+      "Prince Shahriyar"
+    ],
+    "correct": "2",
+    "quesimage": "",
+    "exptext": "<b>Prince Khurram</b><br><br>During Jahangir's reign, the <b>Noor Jahan group</b> was influential, which included her (Noor Jahan's) father <b>Itmaduddaula</b>, mother <b>Asmat Begum</b>, brother <b>Asaf Khan</b> and Prince <b>Khurram</b>.<br><br>Nur Jahan married her daughter <b>Ladli Begum</b> (daughter of her first husband <b>Sher Afghan</b>) to <b>Shahriyar</b>, one of Jahangir's sons, and promoted Shahriyar in her group in place of Khurram.<br><br>It is believed that Nur Jahan signed royal orders along with the emperor.",
+    "expimage": ""
+  },
+  "6": {
+    "questext": "In 1615 CE, which Mewar ruler surrendered to the Mughals, after which Prince Khurram was awarded the title 'Shah Khurram'?",
+    "options": [
+      "Rana Pratap Singh",
+      "Rana Amar Singh",
+      "Rana Sanga",
+      "Rana Udai Singh"
+    ],
+    "correct": "2",
+    "quesimage": "",
+    "exptext": "<b>Rana Amar Singh</b><br><br>In <b>1615 CE</b>, the ruler of <b>Mewar</b>, <b>Rana Amar Singh</b> surrendered. After the success of the Mewar campaign, <b>Khurram</b> was given the title of <b>Shah Khurram</b>.<br><br>During Jahangir's period also, like Akbar, matrimonial relations with Rajput kings continued and <b>Rajputs</b> were given high positions in the Mughal court.",
+    "expimage": ""
+  },
+  "7": {
+    "questext": "Regarding the conquest of Kangra in 1620 CE under Prince Khurram, how did Jahangir celebrate the victory of the Kangra fort?",
+    "options": [
+      "By slaughtering a cow",
+      "By distributing gold coins",
+      "By building a victory tower",
+      "By releasing prisoners"
+    ],
+    "correct": "1",
+    "quesimage": "",
+    "exptext": "<b>By slaughtering a cow</b><br><br>After laying siege to the fort for <b>4 months</b> under the leadership of Prince <b>Khurram</b>, the <b>Kangra</b> army surrendered (<b>1620 CE</b>).<br><br>Jahangir celebrated the victory of the <b>Kangra fort</b> by <b>slaughtering a cow</b>.",
+    "expimage": ""
+  },
+  "8": {
+    "questext": "Who was the Wazir of Ahmednagar who reorganized the state and regained control over its eastern areas by exploiting differences among Mughal commanders?",
+    "options": [
+      "Adil Shah",
+      "Malik Ambar",
+      "Mahabat Khan",
+      "Shah Abbas"
+    ],
+    "correct": "2",
+    "quesimage": "",
+    "exptext": "<b>Malik Ambar</b><br><br>During Akbar's time, <b>Khandesh</b> and some parts of <b>Ahmednagar</b> state were conquered.<br><br>The Wazir of Ahmednagar was <b>Malik Ambar</b>. He organized the state of Ahmednagar and, taking advantage of the differences among the Mughal commanders in the south, regained control over the eastern areas of Ahmednagar.",
+    "expimage": ""
+  },
+  "9": {
+    "questext": "After the 1617 CE treaty between Ahmednagar and the Mughals, Jahangir gave Khurram which title upon his return, following the title 'Farzand' given by the King of Bijapur?",
+    "options": [
+      "Shahjahan",
+      "Aurangzeb",
+      "Nadir-ul-Asr",
+      "English Khan"
+    ],
+    "correct": "1",
+    "quesimage": "",
+    "exptext": "<b>Shahjahan</b><br><br>In <b>1617 CE</b>, a treaty was signed between <b>Ahmednagar state</b> and the Mughals. The king of <b>Bijapur</b> (<b>Adil Shah</b>) gave valuable gifts to <b>Khurram</b>, and in return Jahangir gave him the title of <b>'Farzand'</b> (son). On Khurram's return, he was given the title of <b>'Shahjahan'</b>.",
+    "expimage": ""
+  },
+  "10": {
+    "questext": "Malik Ambar could not abide by the 1617 treaty for long and rebelled again in 1620 CE; who suppressed this rebellion, and what happened to Ahmednagar in 1622 CE?",
+    "options": [
+      "Suppressed by Mahabat Khan; Ahmednagar remained under Mughal control",
+      "Suppressed by Khurram (Shahjahan); Ahmednagar again went out of Mughal control due to Khurram's own rebellion",
+      "Suppressed by Adil Shah; Ahmednagar was annexed permanently",
+      "Suppressed by Jahangir himself; Malik Ambar was executed"
+    ],
+    "correct": "2",
+    "quesimage": "",
+    "exptext": "<b>Suppressed by Khurram (Shahjahan); Ahmednagar again went out of Mughal control due to Khurram's own rebellion</b><br><br><b>Malik Ambar</b> could not abide by the treaty of <b>1617</b> for long. In <b>1620 CE</b>, he again rebelled against the Mughals, which was suppressed by Prince <b>Khurram (Shahjahan)</b>.<br><br>In <b>1622 CE</b>, <b>Ahmednagar</b> again went out of the control of the Mughals due to the rebellion of Prince <b>Khurram</b>.<br><br>Jahangir sent <b>Mahabat Khan</b> to Ahmednagar to suppress Khurram's rebellion.<br><br><b>Malik Ambar</b> died in <b>1626 CE</b>. Malik Ambar implemented <b>Todarmal's land revenue system</b> in the south.",
+    "expimage": ""
+  },
+  "11": {
+    "questext": "In 1622 CE, Kandahar was snatched from the Mughals by which Persian ruler, bringing it under Iranian control during Jahangir's reign?",
+    "options": [
+      "Nadir Shah",
+      "Shah Abbas",
+      "Shah Tahmasp",
+      "Shah Ismail"
+    ],
+    "correct": "2",
+    "quesimage": "",
+    "exptext": "<b>Shah Abbas</b><br><br>In <b>1622 CE</b>, <b>Shah Abbas</b> of Persia snatched <b>Kandahar</b> from the Mughals again. As a result, Kandahar came under the control of the <b>Iranians</b> during the reign of <b>Jahangir</b>.",
+    "expimage": ""
+  },
+  "12": {
+    "questext": "Regarding Jahangir's religious policy, which institution did he abolish while continuing the policy of 'Sulh-e-Kul', and which festival did he celebrate in 1612 CE?",
+    "options": [
+      "Abolished Din-e-Ilahi; celebrated Raksha Bandhan",
+      "Abolished Mansabdari system; celebrated Navroz",
+      "Abolished Jizya tax; celebrated Diwali",
+      "Abolished Sulh-e-Kul; celebrated Holi"
+    ],
+    "correct": "1",
+    "quesimage": "",
+    "exptext": "<b>Abolished Din-e-Ilahi; celebrated Raksha Bandhan</b><br><br>Jahangir abolished <b>'Din-e-Ilahi'</b>. Though he continued the policy of <b>'Sulh-e-Kul'</b>.<br><br>Celebrated the festival of <b>Raksha Bandhan in 1612 CE</b> and got a Rakhi tied on his wrist.",
+    "expimage": ""
+  },
+  "13": {
+    "questext": "Which of the following actions reflects Jahangir's intolerant religious measures against various communities?",
+    "options": [
+      "He punished Brahmins of Rajouri, demolished the Varaha temple of Ajmer, and punished the Jains of Gujarat",
+      "He built new temples in Kashmir for the Brahmins",
+      "He exempted all communities from taxation",
+      "He granted land grants to Jain monks in Gujarat"
+    ],
+    "correct": "1",
+    "quesimage": "",
+    "exptext": "<b>He punished Brahmins of Rajouri, demolished the Varaha temple of Ajmer, and punished the Jains of Gujarat</b><br><br>Jahangir punished the <b>Brahmins of Rajouri (Kashmir)</b>, demolished the <b>Varaha temple of Ajmer</b>, and punished the <b>Jains of Gujarat</b>.",
+    "expimage": ""
+  },
+  "14": {
+    "questext": "Captain Hawkins arrived in Surat in 1608 CE carrying a letter from King James I of England addressed to which Mughal emperor, though he found a different emperor on the throne upon arrival?",
+    "options": [
+      "Addressed to Humayun; found Akbar on the throne",
+      "Addressed to Akbar; found Jahangir on the throne",
+      "Addressed to Jahangir; found Shah Jahan on the throne",
+      "Addressed to Babur; found Humayun on the throne"
+    ],
+    "correct": "2",
+    "quesimage": "",
+    "exptext": "<b>Addressed to Akbar; found Jahangir on the throne</b><br><br>Captain Hawkins and Sir Thomas Roe came to India during the reign of Jahangir to obtain trading permission.<br><br>Hawkins came to India (in <b>Surat</b>) in <b>1608 CE</b>; he brought a letter from <b>King James I of England</b> to the Mughal Emperor <b>Akbar</b> of India. But when he reached India, Emperor Akbar had died and <b>Jahangir</b> had become the king.",
+    "expimage": ""
+  },
+  "15": {
+    "questext": "Captain Hawkins met Jahangir in Agra in 1609 CE and conversed in Persian; with what title did Jahangir honour him?",
+    "options": [
+      "Farzand",
+      "Nadir-ul-Asr",
+      "English Khan",
+      "Itmaduddaula"
+    ],
+    "correct": "3",
+    "quesimage": "",
+    "exptext": "<b>English Khan</b><br><br>Hawkins met <b>Jahangir</b> in <b>Agra (1609 CE)</b> and spoke in <b>Persian</b>. Captain Hawkins was honored by Jahangir with the title of <b>'English Khan'</b>.",
+    "expimage": ""
+  },
+  "16": {
+    "questext": "Sir Thomas Roe, sent by King James I in 1615 CE, succeeded in obtaining what from Jahangir for the British?",
+    "options": [
+      "A 'farman' allowing the British to trade in Surat",
+      "A permanent residency in Agra",
+      "Control over the Kandahar trade route",
+      "A military alliance against the Portuguese"
+    ],
+    "correct": "1",
+    "quesimage": "",
+    "exptext": "<b>A 'farman' allowing the British to trade in Surat</b><br><br>Sir <b>Thomas Roe</b> was sent by <b>James I</b> in <b>1615 CE</b>. Thomas Roe succeeded in obtaining a <b>'farman'</b> from Jahangir to allow the British to trade in <b>Surat</b>.<br><br><b>Note:</b> <b>Francisco Pulsart</b> visited India during the reign of <b>Jahangir</b>.",
+    "expimage": ""
+  },
+  "17": {
+    "questext": "Jahangir died in November 1627 CE at Bhimwar and was buried on the banks of which river, near Lahore?",
+    "options": [
+      "River Yamuna",
+      "River Ravi",
+      "River Sutlej",
+      "River Chenab"
+    ],
+    "correct": "2",
+    "quesimage": "",
+    "exptext": "<b>River Ravi</b><br><br>Jahangir died in <b>November 1627 CE</b> at a place called <b>Bhimwar</b>. Jahangir was buried on the banks of the river <b>Ravi</b> at a place called <b>Shahdara</b> near <b>Lahore</b>.<br><br><b>Alcohol addiction</b> was one of his bad habits, which eventually became the cause of his death.",
+    "expimage": ""
+  },
+  "18": {
+    "questext": "Jahangir's reign is called the golden age of painting; which titles were given to the painters Ustad Mansoor and Abul Hasan respectively?",
+    "options": [
+      "'Nadir-ul-Asr' and 'Nadir-ul-Zaman'",
+      "'English Khan' and 'Farzand'",
+      "'Itmaduddaula' and 'Khan-e-Khana'",
+      "'Shahjahan' and 'Nuruddin'"
+    ],
+    "correct": "1",
+    "quesimage": "",
+    "exptext": "<b>'Nadir-ul-Asr' and 'Nadir-ul-Zaman'</b><br><br>Jahangir's time is called the <b>golden age of painting</b>. <b>Ustad Mansoor</b> and <b>Abul Hasan</b>, the best painters of that time, were given the title of <b>'Nadir-ul-Asr'</b> and <b>'Nadir-ul-Zaman'</b> respectively.<br><br>Other famous painters in the court of Jahangir included:<br>• <b>Abu Hasan</b><br>• <b>Mansur</b><br>• <b>Bishan Das</b><br>• <b>Manohar</b><br>• <b>Goverdhan</b>",
+    "expimage": ""
+  },
+  "19": {
+    "questext": "Which notable tomb did Jahangir alter and partly rebuild as one of his architectural contributions?",
+    "options": [
+      "Humayun's Tomb",
+      "Akbar's Tomb at Sikandra",
+      "Taj Mahal",
+      "Tomb of Sher Shah Suri"
+    ],
+    "correct": "2",
+    "quesimage": "",
+    "exptext": "<b>Akbar's Tomb at Sikandra</b><br><br>Among the notable buildings erected by him, <b>Akbar's tomb at Sikandra</b> is the most remarkable. He altered its design and partly rebuilt it.",
+    "expimage": ""
+  },
+  "20": {
+    "questext": "Itimad-ud-Daulah's tomb near Agra, considered one of the finest buildings of the period, was constructed under the direction of whom?",
+    "options": [
+      "Mumtaz Mahal",
+      "Nur Jahan",
+      "Jahangir",
+      "Shah Jahan"
+    ],
+    "correct": "2",
+    "quesimage": "",
+    "exptext": "<b>Nur Jahan</b><br><br><b>Itimad-ud-Daulah's tomb</b> near <b>Agra</b>, constructed under the direction of <b>Nur Jahan</b>, is one of the finest buildings.<br><br>Some of the gardens in <b>Kashmir (Shalimar Bagh)</b> and <b>Lahore</b> were laid out on his orders.",
+    "expimage": ""
+  },
+  "21": {
+    "questext": "Apart from sentencing Guru Arjun Dev to death, what other religious actions did Jahangir take, as per the note?",
+    "options": [
+      "He imprisoned Sheikh Ahmad Sirhindi and stopped initiation into Din-e-Ilahi",
+      "He freed Sheikh Ahmad Sirhindi and restarted Din-e-Ilahi",
+      "He appointed Sheikh Ahmad Sirhindi as chief advisor",
+      "He built a shrine for Sheikh Ahmad Sirhindi"
+    ],
+    "correct": "1",
+    "quesimage": "",
+    "exptext": "<b>He imprisoned Sheikh Ahmad Sirhindi and stopped initiation into Din-e-Ilahi</b><br><br><b>Note:</b> He sentenced Sikh Guru <b>Arjun Dev</b> to death, imprisoned <b>Sheikh Ahmad Sirhindi</b>, and stopped initiation into <b>Din-e-Ilahi</b>.",
+    "expimage": ""
+  }
+};
